1ef0539320b7e7d1d10df14ff833871ab2469a40
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-24 12:19:58 +0530

    Update Acc_gen.py

--- Miley/modules/Acc_gen.py
+++ Miley/modules/Acc_gen.py
@@ -5,8 +5,12 @@ from Miley.events import register
 @register(pattern="^/nf")
 async def acc(event):
  async with ubot.conversation("@UniqAccGenBot") as conv:
+   try:
       await conv.send_message("/start")
       response = await conv.get_response()
+      await event.reply(response.text)
       await response.click(1)
       k = await response.get_response()
       await event.reply(k.text)
+   except Exception as e:
+      await event.reply(e)
