9dbe6292362ab6be7ab1117bbbd0df7ed5f24f8b
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-24 18:22:49 +0530

    Update _menu.py

--- Miley/modules/_menu.py
+++ Miley/modules/_menu.py
@@ -130,8 +130,8 @@ def get_page(id):
 
 
 def paginate_help(event, page_number, loaded_plugins, prefix):
-    number_of_rows = 8
-    number_of_cols = 2
+    number_of_rows = 15
+    number_of_cols = 3
 
     to_check = get_page(id=event.sender_id)
 
@@ -159,7 +159,7 @@ def paginate_help(event, page_number, loaded_plugins, prefix):
         )
         for x in helpable_plugins
     ]
-    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
@@ -167,8 +167,13 @@ def paginate_help(event, page_number, loaded_plugins, prefix):
     pairs = pairs[
             modulo_page * number_of_rows: number_of_rows * (modulo_page + 1)
         ] + [
+            (
                 custom.Button.inline(
                     "Go Back", data="reopen_again"
-                )
+                ),
+                custom.Button.inline(
+                    "Advanced Commands", data="fun_help"
+                ),
+            )
         ]
     return pairs
