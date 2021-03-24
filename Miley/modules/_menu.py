9a3cf2520e10652139c8b55714d941a46d448d0f
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-24 17:52:55 +0530

    Update _menu.py

--- Miley/modules/_menu.py
+++ Miley/modules/_menu.py
@@ -72,3 +72,99 @@ async def reopen_again(event):
     else:
         pass
 
+@tbot.on(events.CallbackQuery(pattern=r"help_menu"))
+async def help_menu(event):
+    buttons = paginate_help(event, 0, CMD_LIST, "helpme")
+    await event.edit(pmt, buttons=buttons)
+
+@tbot.on(events.CallbackQuery(pattern=r"advanced_menu"))
+async def soon(event):
+    buttons=[[Button.inline("About Me", data="about_me"), Button.inline("Commands", data="help_menu"),],[Button.inline("Go Back", data="reopen_again"),],]
+    await event.edit(ad_caption, buttons=buttons)
+
+@tbot.on(events.CallbackQuery(pattern=r"about_me"))
+async def soon(event):
+    buttons=[Button.inline("Go Back", data="soon"),]
+    await event.edit(about, buttons=buttons)
+
+@tbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"us_plugin_(.*)")))
+async def on_plug_in_callback_query_handler(event):
+    plugin_name = event.data_match.group(1).decode("UTF-8")
+    help_string = ""
+    #RoseLoverX
+    #RoseLoverX
+    for i in CMD_LIST[plugin_name]:
+        plugin = plugin_name.replace("_", " ")
+        emoji = plugin_name.split("_")[0]
+        output = str(CMD_HELP[plugin][1])
+        help_string = f"Here is the help for **{emoji}**:\n" + output
+
+    if help_string is None:
+        pass
+    else:
+        reply_pop_up_alert = help_string
+    try:
+        await event.edit(
+            reply_pop_up_alert, buttons=[
+                [Button.inline("Back", data="go_back")]]
+        )
+    except BaseException:
+        pass
+
+@tbot.on(events.CallbackQuery(pattern=r"go_back"))
+async def go_back(event):
+    c = pagenumber.find_one({"id": event.sender_id})
+    number = c["page"]
+    # print (number)
+    buttons = paginate_help(event, number, CMD_LIST, "helpme")
+    await event.edit(pm_caption, buttons=buttons)
+
+def get_page(id):
+    return pagenumber.find_one({"id": id})
+
+
+def paginate_help(event, page_number, loaded_plugins, prefix):
+    number_of_rows = 15
+    number_of_cols = 3
+
+    to_check = get_page(id=event.sender_id)
+
+    if not to_check:
+        pagenumber.insert_one({"id": event.sender_id, "page": page_number})
+
+    else:
+        pagenumber.update_one(
+            {
+                "_id": to_check["_id"],
+                "id": to_check["id"],
+                "page": to_check["page"],
+            },
+            {"$set": {"page": page_number}},
+        )
+
+    helpable_plugins = []
+    for p in loaded_plugins:
+        if not p.startswith("_"):
+            helpable_plugins.append(p)
+    helpable_plugins = sorted(helpable_plugins)
+    modules = [
+        custom.Button.inline(
+            "{}".format(x.replace("_", " ")), data="us_plugin_{}".format(x)
+        )
+        for x in helpable_plugins
+    ]
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
+    if len(modules) % number_of_cols == 1:
+        pairs.append((modules[-1],))
+    max_num_pages = ceil(len(pairs) / number_of_rows)
+    modulo_page = page_number % max_num_pages
+    pairs = pairs[
+            modulo_page * number_of_rows: number_of_rows * (modulo_page + 1)
+        ] + [
+            custom.Button.inline(
+                    "Go Back", data="reopen_again"
+                ),
+        ]
+    return pairs
+
+
