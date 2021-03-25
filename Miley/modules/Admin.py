3aaa448e9bd60efd8fc77d702817d3f03a537a9c
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-25 15:59:15 +0530

    Create Admin.py

---
+++ Miley/modules/Admin.py
@@ -0,0 +1,78 @@
+from Miley import tbot
+
+from telethon.errors import BadRequestError
+from telethon.tl.functions.channels import EditAdminRequest, EditBannedRequest
+from telethon.tl.functions.messages import UpdatePinnedMessageRequest
+from telethon.tl.types import (
+    ChatAdminRights,
+    ChatBannedRights,
+    MessageEntityMentionName,
+)
+
+PP_TOO_SMOL = "`The image is too small`"
+PP_ERROR = "`Failure while processing the image`"
+NO_ADMIN = "`I am not an admin nub nibba!`"
+NO_PERM = (
+    "`I don't have sufficient permissions! This is so sed. Promote Me!`"
+)
+NO_SQL = "`Running on Non-SQL mode!`"
+
+CHAT_PP_CHANGED = "`Chat Picture Changed`"
+CHAT_PP_ERROR = (
+    "`Some issue with updating the pic,`"
+    "`maybe coz I'm not an admin,`"
+    "`or don't have enough rights.`"
+)
+INVALID_MEDIA = "`Invalid Extension`"
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
+UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
+
+@tbot.on(events.NewMessage(pattern="/ban ?(.*)"))
+async def ban(event):
+    chat = await event.get_chat()
+    chat.admin_rights
+    chat.creator
+    user, reason = await get_user_from_event(event)
+    banned = await tbot.get_permissions(event.chat_id, user)
+    pro = user
+    fname = pro.first_name
+    if banned.is_admin:
+        await event.reply("Oh, Yeah? Lets Start Banning Admins.")
+        return
+    if user:
+        pass
+    else:
+        return
+    try:
+        await event.client(EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS))
+    except BadRequestError:
+        await event.reply("I Could't Ban That User Probably Due To Less Permissions.")
+        return
+    if reason:
+        await event.reply(f"Banned {fname} For \nReason: {reason}")
+    else:
+        await event.reply(f"Banned {fname} !")
