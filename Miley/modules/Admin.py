01331de8838f6f605449aa1b064b7cb464299680
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-25 16:42:47 +0530

    Update Admin.py

--- Miley/modules/Admin.py
+++ Miley/modules/Admin.py
@@ -223,12 +223,16 @@ async def promote(promt):
       title = text
     if promt.is_group:
       if not promt.sender_id == OWNER_ID:
-        if not await is_register_admin(promt.input_chat, promt.sender_id):
+        if await is_register_admin(promt.input_chat, promt.sender_id):
+           if not await can_promote_users(message=promt):
+             await promt.reply("You are missing the following rights to use this command:CanPromoteMembers")
+             return
+           else:
+             pass
+        else:
            await promt.reply("Only admins can execute this command!")
            return
-        elif not await can_promote_users(message=promt):
-            await promt.reply("You are missing the following rights to use this command:CanPromoteMembers")
-            return
+        
     else:
         return
 
