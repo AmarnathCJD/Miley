7fbd1e58d27e1cbc949edb869e056bbca6cde696
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-26 18:14:41 +0530

    Create _test.py

---
+++ Evie/modules/_test.py
@@ -0,0 +1,47 @@
+from telethon import *
+frok telethon.tl import *
+from Evie import tbot
+from Evie.events import register
+
+async def get_user_from_event(event):
+    """ Get the user from argument or replied message. """
+    args = event.pattern_match.group(1).split(" ", 1)
+    extra = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            await event.reply("`Pass the user's username, id or reply!`")
+            return
+
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await tbot.get_entity(user_id)
+                return user_obj
+        try:
+            user_obj = await tbot.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.reply(str(err))
+            return None
+
+    return user_obj, extra
+
+@register(pattern="^/test ?(*.)")
+async def test(event):
+ user, reason = await get_user_from_event(event)
+ s = f'{user.id}'
+ if reason:
+    await event.reply(f"{reason}")
+ await event.reply(s)
