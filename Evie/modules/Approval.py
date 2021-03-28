dff9d8efc4f5cf0a541e5089264700a3192154dc
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-28 15:59:57 +0530

    Update Approval.py

--- Evie/modules/Approval.py
+++ Evie/modules/Approval.py
@@ -14,8 +14,6 @@ approved_users = db.approve
 from Evie.function import is_register_admin, can_approve_users
 
 
-
-
 async def get_user_from_event(event):
     """ Get the user from argument or replied message. """
     if event.reply_to_msg_id:
@@ -58,6 +56,7 @@ async def approve(event):
 
     if event.is_group:
         if not await can_approve_users(message=event):
+            await event.reply("You are missing the following rights to use this command:CanChangeInfo")
             return
     else:
         return
@@ -69,43 +68,37 @@ async def approve(event):
         return
     iid = userr.id
 
+
     if await is_register_admin(event.input_chat, iid):
-        await event.reply("Why will I approve an admin ?")
+        await event.reply("User is already admin - locks, blocklists, and antiflood already don't apply to them.")
         return
 
-    if iid == event.sender_id:
-        await event.reply("Why are you trying to approve yourself ?")
-        print("6")
-        return
+    
 
     if event.sender_id == BOT_ID or int(iid) == int(BOT_ID):
-        await event.reply("I am not gonna approve myself")
-        print("7")
+        await event.reply("There's not much point in approving myself.")
         return
 
     chats = approved_users.find({})
     for c in chats:
         if event.chat_id == c["id"] and iid == c["user"]:
-            await event.reply("This User is Already Approved")
+            await event.reply("[{userr.first_name}](tg://user?id={iid}) has been approved in {event.chat.title}! They will now be ignored by automated admin actions like locks, blocklists, and antiflood.")
             return
 
     approved_users.insert_one({"id": event.chat_id, "user": iid})
-    await event.reply("Successfully Approved User")
+    await event.reply("[{userr.first_name}](tg://user?id={iid}) has been approved in {event.chat.title}! They will now be ignored by automated admin actions like locks, blocklists, and antiflood.")
 
 
 @register(pattern="^/disapprove(?: |$)(.*)")
 async def disapprove(event):
     if event.fwd_from:
         return
-    if MONGO_DB_URI is None:
-        return
     chat_id = event.chat.id
     sender = event.sender_id
     reply_msg = await event.get_reply_message()
-    approved_userss = approved_users.find({})
-
     if event.is_group:
         if not await can_approve_users(message=event):
+            await event.reply("You are missing the following rights to use this command:CanChangeInfo")
             return
     else:
         return
@@ -118,26 +111,16 @@ async def disapprove(event):
     iid = userr.id
 
     if await is_register_admin(event.input_chat, iid):
-        await event.reply("Why will I disapprove an admin ?")
-        return
-
-    if iid == event.sender_id:
-        await event.reply("Why are you trying to disapprove yourself ?")
-        print("6")
-        return
-
-    if event.sender_id == BOT_ID or int(iid) == int(BOT_ID):
-        await event.reply("I am not gonna disapprove myself")
-        print("7")
+        await event.reply("This user is an admin, they can't be unapproved.")
         return
 
     chats = approved_users.find({})
     for c in chats:
         if event.chat_id == c["id"] and iid == c["user"]:
             approved_users.delete_one({"id": event.chat_id, "user": iid})
-            await event.reply("Successfully Disapproved User")
+            await event.reply("[{userr.first_name}](tg://user?id={iid}) is no longer approved in {event.chat.title}.")
             return
-    await event.reply("This User isn't approved yet")
+    await event.reply("[{userr.first_name}](tg://user?id={iid}) isn't approved yet")
 
 
 @register(pattern="^/checkstatus(?: |$)(.*)")
