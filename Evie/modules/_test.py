770ee70f9ca9c3be0e81bc098d608c05704cc764
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-28 20:49:01 +0530

    Update _test.py

--- Evie/modules/_test.py
+++ Evie/modules/_test.py
@@ -1,9 +1,20 @@
 from Evie.events import register
+from pymongo import MongoClient
+from Evie import MONGO_DB_URI
 
-@register(pattern="^/sutest")
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["evie"]
+sudo = sudo.black
+
+
+@register(pattern="^/fucker ?(.*)")
 async def surest(event):
-  if True:
-   await event.reply("Test Success")
+  k = event.pattern_match.group(1)
+  jam = sudo.find({})
+  for i in jam:
+     if k == i["user"]:
+      await event.reply("it's a fucker")
   else:
-   await event.reply("test failed")
+      await event.reply("Not a fucker")
   
