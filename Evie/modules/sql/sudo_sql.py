835f59fedfe10543cedd0e513b6bd46b0fb2e956
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-28 22:26:27 +0530

    Create sudo_sql.py

---
+++ Evie/modules/sql/sudo_sql.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
+from Evie.modules.sql import BASE, SESSION
+
+
+class Nightmode(BASE):
+    __tablename__ = "sudousers"
+    iid = Column(String(10), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+
+Sudo.__table__.create(checkfirst=True)
+
+
+def addsudo(iid: str):
+    nightmoddy = Sudo(str(iid))
+    SESSION.add(nightmoddy)
+    SESSION.commit()
+
+
+def rmsudo(iid: str):
+    rmnightmoddy = SESSION.query(Sudo).get(str(iid))
+    if rmnightmoddy:
+        SESSION.delete(rmnightmoddy)
+        SESSION.commit()
+
+
+def get_all_sudo():
+    stark = SESSION.query(Sudo).all()
+    SESSION.close()
+    return stark
+
+
+def is_sudo(iid: str):
+    try:
+        s__ = SESSION.query(Sudo).get(str(iid))
+        if s__:
+            return str(s__.chat_id)
+    finally:
+        SESSION.close()
