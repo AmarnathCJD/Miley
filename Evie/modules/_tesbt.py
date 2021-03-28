95ceb200bcc5f5b07f7c2f44d535b695c4a9d676
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-28 23:05:41 +0530

    Delete _tesbt.py

--- Evie/modules/_tesbt.py
+++
@@ -1,8 +0,0 @@
-from Evie.events import register
-from Evie.modules._dev import sudo
-
-@register(pattern="^/py")
-async def gay(event):
- k = event.sender_id
- if sudo(k):
-   await event.reply("Sudo Spotted yEah")
