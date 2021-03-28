2e877bdf598b497298ddc408784556bee89d1f87
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-28 23:16:41 +0530

    Create test.py

---
+++ Evie/modules/test.py
@@ -0,0 +1,8 @@
+from telethon import event
+from Evie import BOT_ID, tbot
+
+@tbot.on(events.ChatAction)
+async def handler(event):
+    if event.user_added:
+        if event.user_id == BOT_ID:
+           await tbot.send_message(-1001326741686, "Testing")
