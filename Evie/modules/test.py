323dd10c2c34f8ef68f3cf5d6ada79a880e04791
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-28 23:30:46 +0530

    Delete test.py

--- Evie/modules/test.py
+++
@@ -1,8 +0,0 @@
-from telethon import events
-from Evie import BOT_ID, tbot
-
-@tbot.on(events.ChatAction)
-async def handler(event):
-    if event.user_added:
-        if event.user_id == BOT_ID:
-           await tbot.send_message(-1001326741686, "Testing")
