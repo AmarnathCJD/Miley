d44fdbbaeae6efd5bd01e8304ab4461e439b7c7e
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-28 15:27:43 +0530

    Update __init__.py

--- Evie/function/__init__.py
+++ Evie/function/__init__.py
@@ -11,3 +11,14 @@ async def is_admin(event, user):
     except:
         is_mod = False
     return is_mod
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerUser):
+        return True
