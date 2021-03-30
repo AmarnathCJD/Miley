82d84f83f27c2794385a2aa71300504c7cb80d40
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-30 09:55:13 +0530

    Update _gban_gmute.py

--- Evie/modules/_gban_gmute.py
+++ Evie/modules/_gban_gmute.py
@@ -1,11 +1,30 @@
-from Evie import tbot, OWNER_ID, DEV_USERS
+from Evie import tbot, OWNER_ID, DEV_USERS, MONGO_DB_URI
+from pymongo import MongoClient
 from Evie.function import is_admin
 from Evie.modules._dev import sudo
 from Evie.events import register
 
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["evie"]
+gbanned = db.gban
+
+def get_reason(id):
+    return gbanned.find_one({"user": id})
+
 
 @register(pattern="^/gban ?(.*)")
 async def gban(event):
+ if event.fwd_from:
+        return
+ if event.sender_id == OWNER_ID:
+  pass
+ elif event.sender_id in SUDO_USERS:
+  pass
+ elif sudo(event.sender_id):
+  pass
+ else:
+  return
  input = event.pattern_match.group(1)
  if input:
    arg = input.split(" ", 1)
@@ -16,6 +35,22 @@ async def gban(event):
   else:
     iid = arg[0]
     reason = None
+ else:
+   reply_message = await event.get_reply_message()
+   iid = reply_message.sender_id
+   fname = reply_message.sender.first_name
+   if input:
+     reason = input
+   else:
+     reason = None
+ entity = await tbot.get_input_entity(iid)
+ try:
+    r_sender_id = entity.user_id
+ except Exception:
+        await event.reply("Couldn't fetch that user.")
+        return
+ chats = gbanned.find({})
+ if r_sender_id == OWNER_ID:
+  await event.reply("test")
 
-#balance Tomorrow
     
