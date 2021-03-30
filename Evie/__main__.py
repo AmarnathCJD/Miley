ec9e3c0d9bec49189f2762e9546c2e1208110910
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-30 21:13:14 +0530

    Update __main__.py

--- Evie/__main__.py
+++ Evie/__main__.py
@@ -1,11 +1,12 @@
 from sys import argv, exit
-from Evie import tbot
+from Evie import tbot, bot
 from Evie import TOKEN
 
 import Evie.events
 
 try:
     tbot.start(bot_token=TOKEN)
+    bot.start()
 except Exception:
     print("Bot Token Invalid")
     exit(1)
@@ -14,3 +15,4 @@ if len(argv) not in (1, 3, 4):
     tbot.disconnect()
 else:
     tbot.run_until_disconnected()
+
