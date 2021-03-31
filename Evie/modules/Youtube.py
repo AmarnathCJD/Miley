77ea918c8fedab726bd0f75e3d2e15d5fc60add6
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-31 21:41:22 +0530

    Update Youtube.py

--- Evie/modules/Youtube.py
+++ Evie/modules/Youtube.py
@@ -1,4 +1,3 @@
-from googleapiclient.discovery import build
 from youtubesearchpython import VideosSearch
 from Evie import *
 from html import unescape
@@ -10,107 +9,58 @@ from Evie.events import register
 from telethon import events
 
 
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-        return isinstance(
-            (
-                await tbot(functions.channels.GetParticipantRequest(chat, user))
-            ).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerUser):
-        return True
-
-
-async def youtube_search(
-    query, order="relevance", token=None, location=None, location_radius=None
-):
-    """ Do a YouTube search. """
-    youtube = build(
-        "youtube", "v3", developerKey=YOUTUBE_API_KEY, cache_discovery=False
-    )
-    search_response = (
-        youtube.search()
-        .list(
-            q=query,
-            type="video",
-            pageToken=token,
-            order=order,
-            part="id,snippet",
-            maxResults=10,
-            location=location,
-            locationRadius=location_radius,
-        )
-        .execute()
-    )
-
-    videos = []
-
-    for search_result in search_response.get("items", []):
-        if search_result["id"]["kind"] == "youtube#video":
-            videos.append(search_result)
-    try:
-        nexttok = search_response["nextPageToken"]
-        return (nexttok, videos)
-    except HttpError:
-        nexttok = "last_page"
-        return (nexttok, videos)
-    except KeyError:
-        nexttok = "KeyError, try again."
-        return (nexttok, videos)
-
-
-@register(pattern="^/yts (.*)")
-async def yts_search(video_q):
-
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
-    if video_q.is_group:
-        if await is_register_admin(video_q.input_chat, video_q.message.sender_id):
-            pass
-        elif video_q.chat_id == iid and video_q.sender_id == userss:
-            pass
-        else:
+@tbot.on(events.InlineQuery(pattern=r"yt (.*)"))
+async def inline_id_handler(event: events.InlineQuery.Event):
+    builder = event.builder
+    k = event.pattern_match.group(1)
+    if ":" in k:
+         testinput,evlin = event.pattern_match.group(1).split(";")
+    else:
+         testinput = event.pattern_match.group(1)
+         evlin = 5
+    urllib.parse.quote_plus(testinput)
+    lund = event.sender_id
+    if lund == lund:
+        results = []
+        search = SearchVideos(f"{testinput}", offset=1, mode="dict", max_results=int(evlin))
+        mi = search.result()
+        moi = mi["search_result"]
+        if search == None:
+            resultm = builder.article(
+                title="No Results.",
+                description="Try Again With correct Spelling",
+                text="**No Matching Found**",
+                buttons=[
+                    [Button.switch_inline("Search Again", query="yt ", same_peer=True)],
+                ],
+            )
+            await event.answer([resultm])
             return
-
-    # For /yts command, do a YouTube search from Telegram.
-    query = video_q.pattern_match.group(1)
-    result = ""
-
-    if not YOUTUBE_API_KEY:
-        await video_q.reply(
-            "`Error: YouTube API key missing! Add it to environment vars or config.env.`"
-        )
-        return
-
-    c = await video_q.reply("```Processing...```")
-
-    full_response = await youtube_search(query)
-    videos_json = full_response[1]
-
-    for video in videos_json:
-        title = f"{unescape(video['snippet']['title'])}"
-        link = f"https://youtu.be/{video['id']['videoId']}"
-        result += f"{title}\n{link}\n\n"
-
-    await c.edit(result)
+        for mio in moi:
+            mo = mio["link"]
+            thum = mio["title"]
+            proboyx = mio["id"]
+            thums = mio["channel"]
+            td = mio["duration"]
+            tw = mio["views"]
+            kekme = f"https://img.youtube.com/vi/{proboyx}/hqdefault.jpg"
+            okayz = f"**Title :** `{thum}` \n**Link :** {mo} \n**Channel :** `{thums}` \n**Views :** `{tw}` \n**Duration :** `{td}`"
+            hmmkek = f"Channel : {thums} \nDuration : {td} \nViews : {tw}"
+            results.append(
+                await event.builder.article(
+                    title=thum,
+                    description=hmmkek,
+                    text=okayz,
+                    buttons=Button.switch_inline(
+                        "Search Again", query="yt ", same_peer=True
+                    ),
+                )
+            )
+        await event.answer(results)
 
 
 @register(pattern="^/ytinfo (.*)")
 async def yts_search(video_q):
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
-    if video_q.is_group:
-        if await is_register_admin(video_q.input_chat, video_q.message.sender_id):
-            pass
-        elif video_q.chat_id == iid and video_q.sender_id == userss:
-            pass
-        else:
-            return
     query = video_q.pattern_match.group(1)    
     videosSearch = VideosSearch(query, limit = 1)  
     h = videosSearch.result()
@@ -139,7 +89,8 @@ file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
 __help__ = """
- - /yts <query>: Searches your query in youtube and returns results
+ - Inline Youtube Search
+**Syntax:** @MissEvie_Robot yt <query>:<max results(optional)>
  - /ytinfo <video link>: Returns information about the youtube video
 """
 
