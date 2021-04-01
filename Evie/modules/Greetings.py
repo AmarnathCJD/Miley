a0acd82dbd39c965d7887fc0b4a93006936dc07f
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-01 16:44:39 +0530

    Update Greetings.py

--- Evie/modules/Greetings.py
+++ Evie/modules/Greetings.py
@@ -24,7 +24,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
 db = client["evie"]
 botcheck = db.checkbot
-verified_user = db.user_verifi
+verified_user = db.user_verified
 
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
