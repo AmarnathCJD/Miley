0bd020a91dec9fd126b1f5dbd8ae29514fa8fe08
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-01 14:31:14 +0530

    Update Misc.py

--- Evie/modules/Misc.py
+++ Evie/modules/Misc.py
@@ -14,6 +14,7 @@ from Evie.events import register
 from pymongo import MongoClient
 
 from Evie.modules.sql.setbio_sql import SUDO_USERS as boss
+from Evie.modules.sql.setbio_sql import set_bio, rm_bio
 from Evie import tbot, OWNER_ID, CMD_HELP, ubot, StartTime, MONGO_DB_URI, BOT_ID
 import datetime, time
 from Evie.function import bio
@@ -120,21 +121,25 @@ def get_readable_time(seconds: int) -> str:
 
 @register(pattern="^/setbio ?(.*)")
 async def bio(event):
-  replied_user = await get_user(event)
-  user_id = replied_user.user.id
-  from Evie.modules.sql.setbio_sql import set_bio, rm_bio
+  if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_id = previous_message.sender_id
+  else:
+    user_id = event.sender_id
   input = event.pattern_match.group(1)
-  if input == "None":
-   rm_bio(user_id)
-   await event.reply("Bio Removed sucessfully")
-   return
+  if not input:
+   if event.sender_id == OWNER_ID:
+     rm_bio(user_id)
+     await event.reply("Bio Removed sucessfully")
+     return
   if not event.sender_id == OWNER_ID:
     if not user_id == event.sender_id:
       set_bio(user_id, str(input))
       await event.reply(f"Sucessfully Set Bio of {replied_user.user.first_name} to {input}")
     else:
       await event.reply("Are you looking to change your own ... ?? No can do.")
-
+  else:
+    set_bio(user_id, str(input))
 
 @register(pattern="^/info(?: |$)(.*)")
 async def who(event):
