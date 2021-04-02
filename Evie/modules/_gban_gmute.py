84e98adf3e08c2485433a2df18e5af780b624b88
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-02 19:56:04 +0530

    Update _gban_gmute.py

--- Evie/modules/_gban_gmute.py
+++ Evie/modules/_gban_gmute.py
@@ -110,8 +110,8 @@ async def gban(event):
           await event.reply(
                 "This user is already gbanned, I am updating the reason of the gban with your reason."
             )
-          await tbot.send_message(GBANLOGS, "#GBANUPDATE\n**Originated From:** {}\n\n**Sudo:** [{}](tg://user?id={})\n**User:** [{}](tg://user?id={})\n**ID:** `{}`\n**New Reason:** {}".format(
-                                   group, sender, event.sender_id, fname, r_sender_id, r_sender_id, reason))       
+          await tbot.send_message(GBANLOGS, "**Global Ban**\n#UPDATE\n**Originated From: {} {}**\n\n**Sudo:** [{}](tg://user?id={})\n**User:** [{}](tg://user?id={})\n**ID:** `{}`\n**New Reason:** {}".format(
+                                   group, event.chat_id, sender, event.sender_id, fname, r_sender_id, r_sender_id, reason))       
           return
 
  gbanned.insert_one(
