c3f75ed6858f3c1b81ee1d04a63ccd40468a66dc
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-02 22:58:55 +0530

    Update Misc.py

--- Evie/modules/Misc.py
+++ Evie/modules/Misc.py
@@ -76,7 +76,9 @@ async def detail(replied_user, event):
     if username:
       caption += f"Username: {username} \n"
     caption += f'Permalink: <a href="tg://user?id={user_id}">link</a>'
-    
+    if bio(user_id):
+         k = boss[user_id]
+         caption += f'\n\n<b>What others say:</b>\n{k}'
     a = blacklist.find({})
     for i in a:
          if user_id == i["user"]:
