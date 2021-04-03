edecb7e98567f2895f37d74617baf033fd034f3a
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-03 22:52:08 +0530

    Create Filters.py

---
+++ Evie/modules/Filters.py
@@ -0,0 +1,74 @@
+from julia import CMD_HELP, tbot
+import os
+from Evie.events import register
+from Evie.function import is_admin, can_change_info
+import asyncio
+import re
+from telethon.tl import types
+
+from telethon import utils, Button
+from telethon import events
+from Evie.modules.sql.filters_sql import (
+    add_filter,
+    get_all_filters,
+    remove_filter,
+    remove_all_filters,
+)
+
+DELETE_TIMEOUT = 0
+
+TYPE_TEXT = 0
+
+TYPE_PHOTO = 1
+
+TYPE_DOCUMENT = 2
+
+last_triggered_filters = {}  # pylint:disable=E0602
+
+@register(pattern="^/addfilter (.*)")
+async def save(event):
+ if not event.reply_to_msg_id:
+     input = event.pattern_match.group(1)
+     if input:
+       arg = input.split(" ", 1)
+     if len(arg) == 2:
+      name = arg[0]
+      msg = arg[1]
+      snip = {"type": TYPE_TEXT, "text": msg}
+     else:
+      name = arg[0]
+      if not name:
+        await event.reply("You need to give the filter a name!")
+        return
+      await event.reply("You need to give the filter some content!")
+      return
+ else:
+      message = await event.get_reply_message()
+      if not message.media:
+          msg = reply_message.text
+          name = event.pattern_match.group(1)
+          snip = {"type": TYPE_TEXT, "text": msg}
+      else:
+          media = None
+          if isinstance(msg.media, types.MessageMediaPhoto):
+             media = utils.get_input_photo(msg.media.photo)
+             snip["type"] = TYPE_PHOTO
+          elif isinstance(msg.media, types.MessageMediaDocument):
+             media = utils.get_input_document(msg.media.document)
+             snip["type"] = TYPE_DOCUMENT
+          if media:
+             snip["id"] = media.id
+             snip["hash"] = media.access_hash
+             snip["fr"] = media.file_reference
+ add_filter(
+            event.chat_id,
+            name,
+            snip["text"],
+            snip["type"],
+            snip.get("id"),
+            snip.get("hash"),
+            snip.get("fr"),
+        )
+ await event.reply(f"Saved filter `{name}`")
+
+   
