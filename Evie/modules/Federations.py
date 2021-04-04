853ce251776bab94193f234715090043df8a4343
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-04 13:01:25 +0530

    Update Federations.py

--- Evie/modules/Federations.py
+++ Evie/modules/Federations.py
@@ -61,16 +61,47 @@ async def new(event):
  if not name:
   return await event.reply("You need to give your federation a name! Federation names can be up to 64 characters long.")
  if len(name) > 64:
-  return await event.reply("Federation names can only be upto 64 charactors long")
+  return await event.reply("Federation names can only be upto 64 charactors long.")
  fed_id = str(uuid.uuid4())
  fed_name = name
  x = sql.new_fed(event.sender_id, fed_name, fed_id)
  await event.reply(f"Created new federation with FedID: `{fed_id}`.\nUse this ID to join the federation! eg:\n`/joinfed {fed_id}`")
    
- 
- 
+@register(pattern="^/delfed$")
+async def del(event):
+ if not event.is_private:
+  return await event.reply("Delete your federation in my PM - not in a group.")
+ fedowner = sql.get_user_owner_fed_full(event.sender_id)
+ if not fedowner:
+   return await event.reply("It doesn't look like you have a federation yet!")
+ else:
+    for f in fedowner:
+            fed_id = "{}".format(f["fed_id"])
+            name = f["fed"]["fname"]
+    await tbot.send_message(
+            event.chat_id,
+            "Are you sure you want to delete your federation? This action cannot be undone - you will lose your entire ban list, and '{}' will be permanently gone.".format(
+                name
+            ),
+            buttons=[
+                [Button.inline("Delete Federation", data="rmfed_{}".format(fed_id))],
+                [Button.inline("Cancel", data="nada")],
+            ],
+            reply_to=message,
+        )
 
- 
+@tbot.on(events.CallbackQuery(pattern=r"rmfed(\_(.*))"))
+async def delete_fed(event):
+    tata = event.pattern_match.group(1)
+    data = tata.decode()
+    fed_id = data.split("_", 1)[1]
+    delete = sql.del_fed(fed_id)
+    await event.edit("You have deleted your federation! All chats linked to it are now federation-less.")
+
+@tbot.on(events.CallbackQuery(pattern=r"nada"))
+async def delete_fed(event):
+  await event.edit("Federation deletion canceled")
+    
  
 
 
