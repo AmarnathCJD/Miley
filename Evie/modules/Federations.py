be9af52365ceecf14d25c314ece81cd34a3cfacf
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-04 19:56:01 +0530

    Update Federations.py

--- Evie/modules/Federations.py
+++ Evie/modules/Federations.py
@@ -406,7 +406,7 @@ async def _(event):
         return
     name = info["fname"]
     if is_user_fed_owner(fed_id, int(r_sender_id)) is True:
-           return await event.reply(f"I'm not banning a fed admin from their own fed! ({name})")
+           return await event.reply("I'm not banning a fed admin from their own fed! ({name})")
     if is_user_fed_admin(fed_id, int(r_sender_id)) is True:
-           return await event.reply(f"I'm not banning a fed admin from their own fed! ({name})")
+           return await event.reply("I'm not banning a fed admin from their own fed! ({name})")
 
