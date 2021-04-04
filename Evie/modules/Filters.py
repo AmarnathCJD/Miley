7de0d9f25398965377b0a3f3b1d0c944f60b04f7
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-04 10:08:27 +0530

    Update Filters.py

--- Evie/modules/Filters.py
+++ Evie/modules/Filters.py
@@ -226,13 +226,17 @@ file_helpo = file_help.replace("_", " ")
 
 __help__ = """
 **Filters**
+
 Make your chat more lively with filters; The bot will reply to certain words!
+
 Filters are case insensitive; every time someone says your trigger words, Evie will reply something else! can be used to create your own commands, if desired.
+
 **Commands:**
 - /filter <trigger> <reply>: Every time someone says "trigger", the bot will reply with "sentence". For multiple word filters, quote the trigger.
 - /listfilters: List all chat filters.
 - /stop <trigger>: Stop the bot from replying to "trigger".
 - /stopall: Stop **ALL** filters in the current chat. This cannot be undone.
+
 **Examples:**
 - Set a filter:
 -> /filter hello Hello there! How are you?
