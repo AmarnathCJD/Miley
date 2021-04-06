d9ca9b10a4404ff5ff5d7843d5634f0f65a30948
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-06 22:56:33 +0530

    Update fsub_sql.py

--- Evie/modules/sql/fsub_sql.py
+++ Evie/modules/sql/fsub_sql.py
@@ -12,13 +12,11 @@ class FSUB(BASE):
     chat_id = Column(Integer, primary_key=True)
     is_chat = Column(Boolean)
     channel = Column(UnicodeText)
-    start_time = Column(UnicodeText)
 
-    def __init__(self, chat_id, channel="", is_chat=True, start_time=""):
+    def __init__(self, chat_id, channel="", is_chat=True):
         self.chat_id = chat_id
         self.channel = channel
         self.is_chat = is_chat
-        self.start_time = start_time
 
     def __repr__(self):
         return "{}".format(self.chat_id)
@@ -28,12 +26,10 @@ FSUB.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 FSUB_CHATS = {}
-FSUB_CHATSS = {}
 
 
 def is_chat(chat_id):
     return chat_id in FSUB_CHATS
-    return chat_id in FSUB_CHATSS
 
 
 def check_fsub_status(chat_id):
@@ -43,17 +39,15 @@ def check_fsub_status(chat_id):
         SESSION.close()
 
 
-def set_fsub(chat_id, channel, start_time=""):
+def set_fsub(chat_id, channel):
     with INSERTION_LOCK:
         curr = SESSION.query(FSUB).get(chat_id)
         if not curr:
-            curr = FSUB(chat_id, channel, True, start_time)
+            curr = FSUB(chat_id, channel, True)
         else:
             curr.is_chat = True
             curr.channel = channel
-            curr.start_time = start_time
         FSUB_CHATS[chat_id] = channel
-        FSUB_CHATSS[chat_id] = start_time
         SESSION.add(curr)
         SESSION.commit()
 
@@ -64,7 +58,6 @@ def rm_fsub(chat_id):
         if curr:
             if chat_id in FSUB_CHATS:  # sanity check
                 del FSUB_CHATS[chat_id]
-                del FSUB_CHATSS[chat_id]
             SESSION.delete(curr)
             SESSION.commit()
             return True
@@ -75,11 +68,9 @@ def rm_fsub(chat_id):
 
 def __load_fsub_chats():
     global FSUB_CHATS
-    global FSUB_CHATSS
     try:
         all_chat = SESSION.query(FSUB).all()
         FSUB_CHATS = {chat.chat_id: chat.channel for chat in all_chat if chat.is_chat}
-        FSUB_CHATSS = {chat.chat_id: chat.start_time for chat in all_chat if chat.is_chat}
     finally:
         SESSION.close()
 
