4218a5f4ed18b0f97b0761897d49169678d2c958
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-06 22:57:14 +0530

    Update ForceSubscribe.py

--- Evie/modules/ForceSubscribe.py
+++ Evie/modules/ForceSubscribe.py
@@ -3,3 +3,12 @@
 from Evie import tbot
 from Evie.events import register
 import Evie.modules.sql.fsub_sql as sql
+
+
+@register(pattern="^/fsub")
+async def fs(event):
+  args = event.pattern_match.group(1)
+  if args:
+    set_fsub(event.chat_id, args)
+    await event.reply("Set fsub")
+      
