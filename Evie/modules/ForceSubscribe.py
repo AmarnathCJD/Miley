323d2add015450bf7c19d244b96ea15cc66f3154
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-06 23:27:14 +0530

    Update ForceSubscribe.py

--- Evie/modules/ForceSubscribe.py
+++ Evie/modules/ForceSubscribe.py
@@ -5,12 +5,10 @@ from Evie.events import register
 import Evie.modules.sql.fsub_sql as sql
 
 
-@register(pattern="^/fsub")
+@register(pattern="^/fsub ?(.*)")
 async def fs(event):
   args = event.pattern_match.group(1)
   if args:
-    k = f"{event.chat_id}"
-    chat = k.replace("-", "")
     FK = sql.add_channel(event.chat_id, args)
     if FK:
       await event.reply("Set fsub")
