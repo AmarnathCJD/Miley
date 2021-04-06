0baf25c9073c762400a090e360913179b86a7f13
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-06 15:06:06 +0530

    Update Federations.py

--- Evie/modules/Federations.py
+++ Evie/modules/Federations.py
@@ -382,8 +382,9 @@ Fully Written by RoseLoverX
 """
 @tbot.on(events.CallbackQuery(pattern=r"fedadm(\_(.*))"))
 async def smex_fed(event):
-  if not await is_admin(event, event.sender_id):
-     return await event.answer("You need to be an admin to do this")
+  if event.is_group:
+    if not await is_admin(event, event.sender_id):
+      return await event.answer("You need to be an admin to do this")
   await event.edit(buttons=None)
   tata = event.pattern_match.group(1)
   data = tata.decode()
