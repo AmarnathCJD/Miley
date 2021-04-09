483e8a1daa0f617ecee7aa885209e9bb49ff0e96
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 23:25:44 +0530

    Update Pin.py

--- Evie/modules/Pin.py
+++ Evie/modules/Pin.py
@@ -7,5 +7,10 @@ from Evie.events import register
 async def pn(event):
    message = await tbot.get_messages(event.chat_id, ids=types.InputMessagePinned())
    id = message.id
-   chat = event.chat_id.replace("-100", "")
-   await event.reply(f"The pinned message in {event.chat.title} is [here](http://t.me/{chat}/{id})")
+   chat = event.chat_id
+   pro = f'{chat}'
+   omk = pro.replace('-100', '')
+   if event.chat.username:
+     await event.reply(f"The pinned message in {event.chat.title} is [here](http://t.me/{event.chat.username}/{id})")
+   else:
+     await event.reply(f"The pinned message in {event.chat.title} is [here](http://t.me/{omk}/{id})")
