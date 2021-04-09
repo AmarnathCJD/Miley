9fb506c52d66fcc51af20d041af03b3404876e0e
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 23:20:37 +0530

    Create Pin.py

---
+++ Evie/modules/Pin.py
@@ -0,0 +1,10 @@
+from Evie import tbot, CMD_HELP
+import os
+from telethon import types
+
+@register(pattern="^/pinned")
+async def pn(event):
+   message = await tbot.get_messages(event.chat_id, ids=types.InputMessagePinned())
+   id = message.id
+   chat = event.chat_id.replace("-100", "")
+   await event.reply(f"The pinned message in {event.chat.title} is [here](http://t.me/{chat}/{id})")
