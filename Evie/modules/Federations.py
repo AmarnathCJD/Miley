6323380feb05c58d4770c6ea5bb228acf62e9d9c
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 18:23:14 +0530

    Update Federations.py

--- Evie/modules/Federations.py
+++ Evie/modules/Federations.py
@@ -952,11 +952,7 @@ async def smex_fed(event):
   info = sql.get_fed_info(fed_id)
   name = info["fname"]
   print(fed_id)
-  res = sql.tr_fed(fed_id, int(user))
-  if res:
-    text = f"Congratulations! Federation {name} (`{fed_id}`) has successfully been transferred from [{fname}](tg://user?id={user}) to [{dname}](tg://user?id={owner})"
-    await event.edit(text, buttons=None)
-
+  await event.reply(fed_id)
 
 @tbot.on(events.CallbackQuery(pattern=r"smewxy(\_(.*))"))
 async def smex(event):
