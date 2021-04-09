33759955d3e36f6b88cb64ea23f2da3d93d1a3f8
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 15:28:47 +0530

    Update feds_sql.py

--- Evie/modules/sql/feds_sql.py
+++ Evie/modules/sql/feds_sql.py
@@ -4,7 +4,7 @@ from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
 class Federations(BASE):
-    __tablename__ = "fep"
+    __tablename__ = "fp"
     owner_id = Column(String(14))
     fed_name = Column(UnicodeText)
     fed_id = Column(UnicodeText, primary_key=True)
