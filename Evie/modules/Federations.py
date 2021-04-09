ecb37e91d9b32c18360e082f49baf7f7e6431250
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 20:28:31 +0530

    Update Federations.py

--- Evie/modules/Federations.py
+++ Evie/modules/Federations.py
@@ -926,7 +926,7 @@ async def smex_fed(event):
   if not event.sender_id == int(user):
     return await event.answer("This action is not intended for you!.")
   text = f"[{dname}](tg://user?id={cname}), please confirm that you wish to send fed {name} (`{fed_id}`) to [{fname}](tg://user?id={user}). This cannot be undone."
-  bc = f"{cname}|{fed_id}|{user}"
+  bc = f"{cname}|{user}"
   cb = f"{cname}|{user}"
   buttons = [Button.inline('Confirm', data="pekxd_{}".format(bc)),Button.inline('Cancel', data="dkxd_{}".format(cb))]
   await event.edit(text, buttons=buttons)
@@ -943,26 +943,22 @@ async def smex_fed(event):
   tata = event.pattern_match.group(1)
   data = tata.decode()
   input = data.split("_", 1)[1]
-  user, fed, owner= input.split("|")
+  user, owner= input.split("|")
   user = user.strip()
-  fed_id = fed.strip()
   owner = owner.strip()
   if not event.sender_id == int(user):
    return await event.answer("This action is not intended for you!.")
   rt = await tbot(GetFullUserRequest(int(user)))
   tr = await tbot(GetFullUserRequest(int(owner)))
   fname = rt.user.first_name
-  fedowner = sql.get_user_owner_fed_full(int(user))
+  fedowner = sql.get_user_owner_fed_full(event.sender_id)
+  if not fedowner:
+        return await event.reply("Some error occurred during fed transfer process!")
   for f in fedowner:
-            fed_id = f["fed_id"]
-            name = f["fed"]["fname"]
+          fed_id = f["fed_id"]
+          name = f["fed"]["fname"]
   dname = tr.user.first_name
   res = sql.tr_fed(fed_id, user)
-  cmd = f'Evie.modules.sql.feds_sql.tr_fed("{fed_id}, {user}")'
-  try:
-      await aexec(cmd, event)
-  except Exception as e:
-      await event.reply(e)
   if res:
     text = f"Congratulations! Federation {name} ({fed_id}) has successfully been transferred from [{fname}](tg://user?id={user}) to [{dname}](tg://user?id={owner})"
     await event.edit(text, buttons=None)
