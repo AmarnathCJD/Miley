085fb19b525b6c1d8576bad9ff6e8996f050a878
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 22:29:33 +0530

    Update Song.py

--- Evie/modules/Song.py
+++ Evie/modules/Song.py
@@ -34,7 +34,7 @@ async def yt(event):
     await asyncio.sleep(0.2)
     downloaded_thumb = wget.download(thumb_url)
     image = Image.open(downloaded_thumb)
-    new_image = image.resize((20, 25))
+    new_image = image.resize((1920, 1080))
     new_image.save('image69.jpg')
     thumb = './image69.jpg'
     opts = {
