7e30f04bc80bfa8132324af9fe1bac7bec93330a
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 20:58:49 +0530

    Update Misc.py

--- Evie/modules/Misc.py
+++ Evie/modules/Misc.py
@@ -172,6 +172,28 @@ async def ping(event):
         parse_mode="html",
     )
 
+
+@ubot.on(events.NewMessage(pattern="!ping"))
+async def ubot(event):
+    if not event.sender_id == OWNER_ID:
+        return
+    if event.fwd_from:
+        return
+    start_time = datetime.datetime.now()
+    message = await event.edit("Pinging.")
+    end_time = datetime.datetime.now()
+    pingtime = end_time - start_time
+    await message.edit("Pinging..")
+    telegram_ping = str(round(pingtime.total_seconds(), 2)) + "s"
+    uptime = get_readable_time((time.time() - StartTime))
+    await message.edit(
+        "PONG !\n"
+        "<b>Time Taken:</b> <code>{}</code>\n"
+        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        parse_mode="html",
+    )
+    
+
 """RoseLoverX"""
 from telethon import events
 from telethon.tl import functions
