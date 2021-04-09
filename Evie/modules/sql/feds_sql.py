9f39b5ac126e7a6cddc462087578a85464bc75d5
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-09 15:56:16 +0530

    Update feds_sql.py

--- Evie/modules/sql/feds_sql.py
+++ Evie/modules/sql/feds_sql.py
@@ -315,24 +315,28 @@ def tr_fed(fed_id, user_id):
         except ValueError:
             return False
         owner += (int(user_id))
+        
         print(owner)
         fed.owner_id = user_id
         # Set user
-        oldname = FEDERATION_BYFEDID[str(fed_id)]["owner"]
-        tempdata = FEDERATION_BYOWNER[oldname]
-        FEDERATION_BYOWNER.pop(oldname)
-        FEDERATION_BYNAME[str(fed_name)]["owner"] = user_id
-        FEDERATION_BYFEDID[str(fed_id)]["owner"]= user_id
-        FEDERATION_BYOWNER[str(user_id)] = tempdata
-        FEDERATION_BYOWNER[str(user_id)]["fusers"] = str(
+        try:
+          oldname = FEDERATION_BYFEDID[str(fed_id)]["owner"]
+          tempdata = FEDERATION_BYOWNER[oldname]
+          FEDERATION_BYOWNER.pop(oldname)
+          FEDERATION_BYNAME[str(fed_name)]["owner"] = user_id
+          FEDERATION_BYFEDID[str(fed_id)]["owner"]= user_id
+          FEDERATION_BYOWNER[str(user_id)] = tempdata
+          FEDERATION_BYOWNER[str(user_id)]["fusers"] = str(
             {"owner": str(user_id), "members": str(members)}
-        )
-        FEDERATION_BYFEDID[str(fed_id)]["fusers"] = str(
+          )
+          FEDERATION_BYFEDID[str(fed_id)]["fusers"] = str(
             {"owner": str(user_id), "members": str(members)}
-        )
-        FEDERATION_BYNAME[fed_name]["fusers"] = str(
+          )
+          FEDERATION_BYNAME[fed_name]["fusers"] = str(
             {"owner": str(user_id), "members": str(members)}
-        )
+          )
+        except Exception as e:
+            print(e)
         # Set on database
         fed = Federations(
             str(user_id),
