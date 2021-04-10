a1f9fd4c7fabdcc33b30e4ccf8743c0fdfaff1c0
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-10 09:04:47 +0530

    Update Pin.py

--- Evie/modules/Pin.py
+++ Evie/modules/Pin.py
@@ -1,7 +1,21 @@
 from Evie import tbot, CMD_HELP
 import os
-from telethon import types
+from telethon import types, functions, Button
 from Evie.events import register
+from Evie.function import is_admin
+
+
+async def can_pin_msg(message):
+    result = await tbot(
+        functions.channels.GetParticipantRequest(
+            channel=message.chat_id,
+            user_id=message.sender_id,
+        )
+    )
+    p = result.participant
+    return isinstance(p, types.ChannelParticipantCreator) or (
+        isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.pin_messages
+    )
 
 @register(pattern="^/pinned")
 async def pn(event):
@@ -14,3 +28,17 @@ async def pn(event):
      await event.reply(f"The pinned message in {event.chat.title} is [here](http://t.me/{event.chat.username}/{id}).", link_preview=False)
    else:
      await event.reply(f"The pinned message in {event.chat.title} is [here](http://t.me/{omk}/{id}).", link_preview=False)
+
+@register(pattern="^/unpinall")
+async def upinall(event):
+  if not await is_admin(event, event.sender_id):
+    return await event.reply("You need to be an admin to do this!")
+  if not await can_pin_msg(event):
+    return await event.reply("You are missing the following rights to use this command:CanPinMessages")
+  text = "Are you sure you want to unpin all messages?"
+  buttons = [Button.inline('Yes', data='upin_{}'.format(mc)),Button.inline('No', data='cpin_{}'.format(bc))]
+  await tbot.send_message(
+                event.chat_id,
+                text,
+                buttons=buttons)
+    
