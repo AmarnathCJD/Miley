9b2612a454bc0c99a93e9929c8bb1320ec5bf997
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-10 18:22:37 +0530

    Update Song.py

--- Evie/modules/Song.py
+++ Evie/modules/Song.py
@@ -4,7 +4,7 @@ import asyncio, wget, time, requests, os
 from youtube_dl import YoutubeDL
 from PIL import Image
 from youtubesearchpython import SearchVideos
-from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeAudio
+from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 
 @register(pattern="^/song ?(.*)")
 async def yt(event):
@@ -92,63 +92,89 @@ async def yt(event):
   await event.reply(f"{e}")
 
 @register(pattern="^/video ?(.*)")
-async def deezr(event):
- try:
-    input_str = event.pattern_match.group(1)
-    pablo = await event.reply(f"Processing...")
-    if not input_str:
-        await pablo.edit(
-            "Please Give Me A Valid Input."
+async def deezr(v_url):
+    url = v_url.pattern_match.group(1)
+    rkp = await v_url.reply("Processing ...")
+    if not url:
+        await rkp.edit("Error \nusage video <song name>")
+    search = SearchVideos(url, offset=1, mode="json", max_results=1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get("search_result")
+    try:
+        url = q[0]["link"]
+    except BaseException:
+        return await rkp.edit("Failed to find that video song")
+    type = "video"
+    await rkp.edit("`Preparing to download ...`")
+    if type == "video":
+        opts = {
+            "format": "best",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
+            ],
+            "outtmpl": "%(id)s.mp4",
+            "logtostderr": False,
+            "quiet": True,
+        }
+        song = False
+        video = True
+    try:
+        await rkp.edit("`Fetching data, please wait ...`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await rkp.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await rkp.edit("The download content was too short.")
+        return
+    except GeoRestrictedError:
+        await rkp.edit(
+            "Video is not available from your geographic location due to geographic restrictions imposed by a website."
         )
         return
-    await pablo.edit(f"Getting {input_str} From Youtube Servers. Please Wait.")
-    search = SearchVideos(str(input_str), offset=1, mode="dict", max_results=1)
-    rt = search.result()
-    result_s = rt["search_result"]
-    url = result_s[0]["link"]
-    vid_title = result_s[0]["title"]
-    yt_id = result_s[0]["id"]
-    uploade_r = result_s[0]["channel"]
-    thumb_url = f"https://img.youtube.com/vi/{yt_id}/hqdefault.jpg"
-    await asyncio.sleep(0.4)
-    downloaded_thumb = wget.download(thumb_url)
-    opts = {
-        "format": "best",
-        "addmetadata": True,
-        "key": "FFmpegMetadata",
-        "prefer_ffmpeg": True,
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
-        "outtmpl": "%(id)s.mp4",
-        "logtostderr": False,
-        "quiet": True,
-    }
-    try:
-        with YoutubeDL(opts) as ytdl:
-            ytdl_data = ytdl.extract_info(url, download=True)
+    except MaxDownloadsReached:
+        await rkp.edit("Max-downloads limit has been reached.")
+        return
+    except PostProcessingError:
+        await rkp.edit("There was an error during post processing.")
+        return
+    except UnavailableVideoError:
+        await rkp.edit("Media is not available in the requested format.")
+        return
+    except XAttrMetadataError as XAME:
+        await rkp.edit(f"{XAME.code}: {XAME.msg}\n{XAME.reason}")
+        return
+    except ExtractorError:
+        await rkp.edit("`There was an error during info extraction.`")
+        return
     except Exception as e:
-        await pablo.edit(f"**Failed To Download** \n**Error :** `{str(e)}`")
+        await rkp.edit(f"{str(type(e)): {str(e)}}")
         return
     c_time = time.time()
-    file_stark = f"{ytdl_data['id']}.mp4"
-    await pablo.edit(f"Preparing to upload Video:\n**{vid_title}**\nby **{uploade_r}**")
-    async with tbot.action(event.chat_id, 'video'):
-       await tbot.send_file(
-        event.chat_id,
-        open(file_stark, "rb"),
-        thumb=downloaded_thumb,
-        supports_streaming=True,
-        force_document=False,
-        attributes=[
+    if video:
+        await rkp.edit(f"Sending the video song ...")
+
+        y = await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp4",
+            supports_streaming=True,
+            attributes=[
                 DocumentAttributeVideo(
-                    duration=int(ytdl_data["duration"]),
-                )
-            ],
-    )
-    await pablo.delete()
-    for files in (downloaded_thumb, file_stark):
-        if files and os.path.exists(files):
-            os.remove(files)
- except Exception as e:
-   print(e)
+                    duration=int(rip_data["duration"]),
+             )
+             ],
+            caption=rip_data["title"],
+        )
+        try:
+            os.system("rm -rf *.mp4")
+            os.system("rm -rf *.webp")
+            os.system("rm -rf *.jpg")
+        except:
+           pass
