20a2cc86e9e08e24a05691a76d1fea2ff40d7583
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-10 21:23:22 +0530

    Update _admin.py

--- Evie/modules/_admin.py
+++ Evie/modules/_admin.py
@@ -1,13 +1,13 @@
-
-from Evie.
+from Evie import tbot, OWNER_ID
+from Evie.function import is_admin, can_ban_users
 
 @tbot.on(events.NewMessage(pattern="^[!/]dban$"))
 async def dban(event): 
-  if not event.sender_id == 1087968824 or event.sender_id == 1704673514:
+  if not event.sender_id == OWNER_ID:
     if not await user_is_admin(event, event.sender_id):
        return await event.reply("Only Admins can execute this command!")
     if not await can_ban_users(message=event):
-        await event.reply("I don't have enough rights to do that!")
+        await event.reply("You don't have enough rights to do that!")
         return
     reply_msg = await event.get_reply_message()
     if not reply_msg:
@@ -16,5 +16,5 @@ async def dban(event):
     x = (await event.get_reply_message()).sender_id
     zx = (await event.get_reply_message())
     await zx.delete()
-    await telethn(EditBannedRequest(event.chat_id, x, ChatBannedRights(until_date=None, view_messages=True)))
+    await tbot(EditBannedRequest(event.chat_id, x, ChatBannedRights(until_date=None, view_messages=True)))
     await event.reply("Successfully Banned!")
