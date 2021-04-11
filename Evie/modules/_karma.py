b9a11698d2f1fdc62de5f3065879a9d45087448e
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-11 14:08:48 +0530

    Update _karma.py

--- Evie/modules/_karma.py
+++ Evie/modules/_karma.py
@@ -3,10 +3,12 @@ from telethon import events
 from telethon.tl.functions.users import GetFullUserRequest
 from Evie.modules.sql.karma_sql import (update_karma, get_karma, get_karmas,
                                    int_to_alpha, alpha_to_int)
+from Evie.events import register
+from Evie.function import is_admin
 
 
 regex_upvote = r"^((?i)\+|\+\+|\+1|thx|tnx|ty|thank you|thanx|thanks|pro|cool|good|👍)$"
-regex_downvote = r"^(\-|\-\-|\-1|👎)$"
+regex_downvote = r"^(\-|\-\-|\-1|👎|Na|Gey|noob)$"
 
 @tbot.on(events.NewMessage(pattern=None))
 async def kk(event):
@@ -40,8 +42,80 @@ async def kk(event):
         await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
  await tbot.send_message(event.chat_id, f"Incremented Karma of [{fname}](tg://user?id={user_id}) By 1 \nTotal Points: {karma}")
  
+
+@tbot.on(events.NewMessage(pattern=None))
+async def rv(event):
+ if event.is_private:
+   return
+ if event.text == None:
+   return
+ if event.text in regex_downvote:
+   pass
+ else:
+   return
+ if not event.reply_to_msg_id:
+   return
+ previous_message = await event.get_reply_message()
+ user_id = previous_message.sender_id
+ if not event.sender_id == OWNER_ID:
+   if event.sender_id == user_id or user_id == BOT_ID:
+      return
+ arg = await tbot(GetFullUserRequest(user_id))
+ fname = arg.user.first_name
+ chat_id = int(event.chat_id)
+ current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
+ if current_karma:
+        current_karma = current_karma['karma']
+        karma = current_karma - 1
+        new_karma = {"karma": karma}
+        await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+ else:
+        karma = 1
+        new_karma = {"karma": karma}
+        await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+ await tbot.send_message(event.chat_id, f"Decremented Karma Of [{fname}](tg://user?id={user_id}) By 1 \nTotal Points: {karma}")
  
- 
+@register(pattern="^/karma")
+async def kr(event):
+ chat_id = event.chat_id
+ if event.is_private:
+  return
+ if not await is_admin(event, event.sender_id):
+  return
+ if not event.reply_to_msg_id:
+        karma = await get_karmas(chat_id)
+        msg = f"**Karma list of {message.chat.title}:- **\n"
+        limit = 0
+        karma_dicc = {}
+        for i in karma:
+           user_id = await alpha_to_int(i)
+           user_karma = karma[i]['karma']
+           karma_dicc[str(user_id)] = user_karma
+           karma_arranged = dict(
+           sorted(karma_dicc.items(), key=lambda item: item[1], reverse=True))
+        for user_idd, karma_count in karma_arranged.items():
+            if limit > 9:
+                break
+            try:
+                arg = await tbot(GetFullUserRequest(user_id))
+                user_name = arg.user.username
+            except Exception:
+                continue
+            msg += f"{user_name} : `{karma_count}`\n"
+            limit += 1
+        await event.reply(msg)
+ else:
+  previous_message = await event.get_reply_message()
+  user_id = previous_message.sender_id
+  karma = await get_karma(chat_id, await int_to_alpha(user_id))
+  if karma:
+       karma = karma['karma']
+       await event.reply(f'**Total Points**: __{karma}__')
+  else:
+       karma = 0
+       await event.reply(f'**Total Points**: __{karma}__')
+
+          
  
  
  
