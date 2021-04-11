572f46ddbd23f169d8b44ab84909be3b56c98417
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-11 14:51:56 +0530

    Update karma_sql.py

--- Evie/modules/sql/karma_sql.py
+++ Evie/modules/sql/karma_sql.py
@@ -71,3 +71,45 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
         user_id += str(index)
     user_id = int(user_id)
     return user_id
+
+from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
+from Evie.modules.sql import BASE, SESSION
+
+
+class Nightmode(BASE):
+    __tablename__ = "k_chats"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+
+Nightmode.__table__.create(checkfirst=True)
+
+
+def add_chat(chat_id: str):
+    nightmoddy = Nightmode(str(chat_id))
+    SESSION.add(nightmoddy)
+    SESSION.commit()
+
+
+def rmchat(chat_id: str):
+    rmnightmoddy = SESSION.query(Nightmode).get(str(chat_id))
+    if rmnightmoddy:
+        SESSION.delete(rmnightmoddy)
+        SESSION.commit()
+
+
+def get_all_chat_id():
+    stark = SESSION.query(Nightmode).all()
+    SESSION.close()
+    return stark
+
+
+def is_chat(chat_id: str):
+    try:
+        s__ = SESSION.query(Nightmode).get(str(chat_id))
+        if s__:
+            return str(s__.chat_id)
+    finally:
+        SESSION.close()
