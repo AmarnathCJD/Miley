732d99e1e7603a1ac60a57f3c5c810fafdf0aa93
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-11 16:07:24 +0530

    Update _gban_gmute.py

--- Evie/modules/_gban_gmute.py
+++ Evie/modules/_gban_gmute.py
@@ -45,7 +45,7 @@ async def gban(event):
   pass
  elif event.sender_id in DEV_USERS:
   pass
- elif sudo(id):
+ elif sudo(event.sender_id):
   pass
  else:
   return
@@ -145,7 +145,7 @@ async def ugban(event):
   pass
  elif event.sender_id in DEV_USERS:
   pass
- elif sudo(id):
+ elif sudo(event.sender_id):
   pass
  else:
   return
