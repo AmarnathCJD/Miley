f8068f6592035693673548c15d72b182355fc212
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-11 09:51:17 +0530

    Update _dadmin.py

--- Evie/modules/_dadmin.py
+++ Evie/modules/_dadmin.py
@@ -6,11 +6,14 @@ from telethon import events
 
 @tbot.on(events.NewMessage(pattern="^[!/]dban$"))
 async def dban(event): 
-  x = (await event.get_reply_message()).sender_id
   if not event.sender_id == OWNER_ID:
     if not await user_is_admin(event, event.sender_id):
        return await event.reply("Only Admins can execute this command!")
-    if x:
+    if not event.is_reply:      
+     await event.reply("Reply to someone to delete the message and ban the user!")
+     return
+    if event.is_reply:
+      x = (await event.get_reply_message()).sender_id
       if int(x) == BOT_ID or int(x) == OWNER_ID:
         return await event.reply("Ask the chat creator to do it!")
       if await is_admin(event, x):
@@ -21,9 +24,6 @@ async def dban(event):
   if not await bot_ban(message=event):
     return await event.reply("I don't have enough rights to do this!")
   reply_msg = await event.get_reply_message()
-  if not reply_msg:      
-     await event.reply("Reply to someone to delete the message and ban the user!")
-     return
   zx = (await event.get_reply_message())
   await zx.delete()
   await tbot(EditBannedRequest(event.chat_id, x, ChatBannedRights(until_date=None, view_messages=True)))
