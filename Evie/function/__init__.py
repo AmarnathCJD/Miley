7173f39fc406e096b90c95c113bcce00a7747349
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-11 10:24:59 +0530

    Update __init__.py

--- Evie/function/__init__.py
+++ Evie/function/__init__.py
@@ -92,6 +92,33 @@ def bio(iid):
  else:
   return False
 
+async def get_user(event):
+    """ Get the user from argument or replied message. """
+    args = event.pattern_match.group(1).split(" ", 1)
+    extra = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            await event.reply("`Pass the user's username, id or reply!`")
+            return
+        try:
+            user_obj = await tbot.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.reply(str(err))
+            return None
+
+    return user_obj, extra
+
 
 def time_formatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
