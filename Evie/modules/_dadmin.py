b4d5ae1ce464a27d11c25f5fcc64e2bce42ded08
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-11 10:03:30 +0530

    Update _dadmin.py

--- Evie/modules/_dadmin.py
+++ Evie/modules/_dadmin.py
@@ -54,4 +54,28 @@ async def dban(event):
   await tbot.kick_participant(event.chat_id, x)
   await event.reply("Successfully Kicked!")
 
-
+@tbot.on(events.NewMessage(pattern="^[!/]dmute$"))
+async def dban(event): 
+  if not event.sender_id == OWNER_ID:
+    if not await user_is_admin(event, event.sender_id):
+       return await event.reply("Only Admins can execute this command!")
+    if not event.is_reply:      
+     await event.reply("Reply to someone to delete the message and mute the user!")
+     return
+    if event.is_reply:
+      x = (await event.get_reply_message()).sender_id
+      if int(x) == BOT_ID or int(x) == OWNER_ID:
+        return await event.reply("Ask the chat creator to do it!")
+      if await is_admin(event, x):
+        return await event.reply("Yeah lets start muting admins!")
+    if not await can_ban_users(message=event):
+        await event.reply("You don't have enough rights to do that!")
+        return
+  await event.reply(event.text)
+  if not await bot_ban(message=event):
+    return await event.reply("I don't have enough rights to do this!")
+  reply_msg = await event.get_reply_message()
+  zx = (await event.get_reply_message())
+  await zx.delete()
+  await tbot(EditBannedRequest(event.chat_id, x, ChatBannedRights(until_date=None, send_messages=True)))
+  await event.reply("Successfully Muted!")
