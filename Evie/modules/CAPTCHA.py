c8572d239d3107995748e739d0dc2c72834e57d4
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-12 22:48:52 +0530

    Create CAPTCHA.py

---
+++ Evie/modules/CAPTCHA.py
@@ -0,0 +1,21 @@
+from Evie import tbot, CMD_HELP
+import os, asyncio
+
+"""
+async def kick_restricted_after_delay(delay, event, user_id):
+    """If the new member is still restricted after the delay, delete
+    button message and join message and then kick him
+    """
+    await asyncio.sleep(delay)
+    join_message = event.get_reply_message()
+    group_chat = event.chat_id
+    user_id = user_id
+    await join_message.delete()
+    await event.delete()
+    await _ban_restricted_user_until_date(group_chat, user_id, duration=delay).
+
+@tbot.on(events.ChatAction())  # pylint:disable=E0602
+async def _(event):
+  if event.user_joined:
+"""
+#balance soon
