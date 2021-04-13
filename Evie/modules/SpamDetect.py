3ee936be2b9668cc27d1abe45a70b5b8f7f65e59
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-13 13:14:17 +0530

    Update SpamDetect.py

--- Evie/modules/SpamDetect.py
+++ Evie/modules/SpamDetect.py
@@ -1,1 +1,12 @@
 #Soon!
+from Evie import tbot
+from Evie.function import is_nsfw
+@tbot.on(events.NewMessage())        
+async def ws(event):
+ if event.is_private:
+   return
+ if not event.photo:
+   return
+ hmmstark = await is_nsfw(event)
+ if hmmstark is True:
+   await event.delete()
