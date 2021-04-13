5db222a032423494ccfed41599d866d368498931
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-13 14:37:47 +0530

    Update CAPTCHA.py

--- Evie/modules/CAPTCHA.py
+++ Evie/modules/CAPTCHA.py
@@ -1,6 +1,13 @@
-from Evie import tbot, CMD_HELP
+from Evie import tbot, CMD_HELP, MONGO_DB_URI
 import os, asyncio
-
+from telethon import Button, events
+from random import shuffle
+from pyrogram import emoji
+from pymongo import MongoClient
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["evie"]
+captcha = db.captcha
 """
 async def kick_restricted_after_delay(delay, event, user_id):
     
@@ -11,9 +18,50 @@ async def kick_restricted_after_delay(delay, event, user_id):
     await join_message.delete()
     await event.delete()
     await _ban_restricted_user_until_date(group_chat, user_id, duration=delay).
-
+"""
 @tbot.on(events.ChatAction())  # pylint:disable=E0602
 async def _(event):
-  if event.user_joined:
-"""
-#balance soon
+  if not event.user_joined:
+          return
+  chats = captcha.find({})
+  for c in chats:
+       if not event.chat_id == c["id"]:
+          return
+       if event.chat_id == c["id"]:
+          type = c["type"]
+  if not type == "button":
+     return
+  a_user = await event.get_user()
+  mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+  text = (f"Welcome, {mention}\nAre you human?\n\nClick on the button which include this emoji {emoji.CHECK_MARK_BUTTON}.")
+  keyboard = [
+            Button.inline(
+                f"{emoji.BRAIN}",
+                data="fk"
+            ),
+            Button.inline(
+                f"{emoji.CHECK_MARK_BUTTON}",
+                data='pro'
+            ),
+            Button.inline(
+                f"{emoji.CROSS_MARK}",
+                data="fk"
+            ),
+            Button.inline(
+                f"{emoji.ROBOT}",
+                data='yu'
+            )
+        ]
+  shuffle(keyboard)
+  button_message = await event.reply(
+            text,
+            buttons=keyboard
+        )
+  
+  
+  
+    
+          
+  
+  
+  
