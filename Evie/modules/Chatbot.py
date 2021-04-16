62212af989c254988bc3e136b23447fdde5d3ecc
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-16 12:34:47 +0530

    Update Chatbot.py

--- Evie/modules/Chatbot.py
+++ Evie/modules/Chatbot.py
@@ -1,141 +1,108 @@
-from Evie import CMD_HELP, BOT_ID, tbot, LYDIA_API_KEY, OWNER_ID
-import os
-from time import time
-import asyncio
-import Evie.modules.sql.chatbot_sql as sql
-import Evie.modules.sql.ai_sql as ly
+# Made by RoseLoverX
+# Multi language added by @InukaASiTH
 
-from coffeehouse.api import API
-from coffeehouse.exception import CoffeeHouseError as CFError
-from coffeehouse.lydia import LydiaAI
-from telethon import types
-from telethon.tl import functions
-from Evie.events import register
+from Evie import tbot, OWNER_ID, BOT_ID
+import Evie.modules.sql.ai_sql as sql
+from google_trans_new import google_translator
+translator = google_translator()
+import requests
+
 from telethon import events
+from Evie.events import register
 
-CoffeeHouseAPI = API(LYDIA_API_KEY)
-api_client = LydiaAI(CoffeeHouseAPI)
-
-
-async def can_change_info(message):
-    try:
-        result = await tbot(
-            functions.channels.GetParticipantRequest(
-                channel=message.chat_id,
-                user_id=message.sender_id,
-            )
-        )
-        p = result.participant
-        return isinstance(p, types.ChannelParticipantCreator) or (
-            isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.change_info
-        )
-    except Exception:
-        return False
-
+string = (
+  "I belong To RoseLoverX!",
+  "Im Fairly Yound And Was Made by RoseLover!",
+)
+from Evie.function import can_change_info
 
 @register(pattern="^/addchat$")
 async def _(event):
     if event.is_group:
-        if not await can_change_info(message=event):
-            return
+        if not event.sender_id == OWNER_ID:
+           if not await can_change_info(message=event):
+              return
     else:
         return
-
-    global api_client
     chat = event.chat
-    send = await event.get_sender()
-    user = await tbot.get_entity(send)
     is_chat = sql.is_chat(chat.id)
-    k = ly.is_chat(chat.id)
-    if k:
-        ly.rem_chat(chat.id)
     if not is_chat:
-        ses = api_client.create_session()
-        ses_id = str(ses.id)
-        expires = str(ses.expires)
+        ses_id = 'null'
+        expires = 'null'
         sql.set_ses(chat.id, ses_id, expires)
         await event.reply("AI successfully enabled for this chat!")
         return
-    await event.reply("AI is already enabled for this chat!")
-    return ""
-
+    await event.reply("AI Bot is already enabled for this chat!")
 
 @register(pattern="^/rmchat$")
 async def _(event):
     if event.is_group:
-        if not await can_change_info(message=event):
-            return
+        if not event.sender_id == OWNER_ID:
+          return
     else:
         return
     chat = event.chat
-    send = await event.get_sender()
-    user = await tbot.get_entity(send)
     is_chat = sql.is_chat(chat.id)
     if not is_chat:
         await event.reply("AI isn't enabled here in the first place!")
-        return ""
-    sql.rem_chat(chat.id)
-    await event.reply("AI disabled successfully!")
-
-
-@tbot.on(events.NewMessage(pattern=None))
-async def check_message(event):
-    if event.is_group:
-        pass
-    else:
         return
-    message = str(event.text)
-    reply_msg = await event.get_reply_message()
-    if message.lower() == "evie":
-        return True
-    if reply_msg:
-        if reply_msg.sender_id == BOT_ID:
-            return True
-    else:
-        return False
-
+    sql.rem_chat(chat.id)
+    await event.reply("AI Bot disabled successfully!")
 
 @tbot.on(events.NewMessage(pattern=None))
 async def _(event):
-    if event.is_group:
-        pass
-    else:
-        return
-    global api_client
-    msg = str(event.text)
-    chat = event.chat
-    is_chat = sql.is_chat(chat.id)
-    if not is_chat:
-        return
-    if msg:
-        if not await check_message(event):
-            return
-        sesh, exp = sql.get_ses(chat.id)
-        query = msg
-        try:
-            if int(exp) < time():
-                ses = api_client.create_session()
-                ses_id = str(ses.id)
-                expires = str(ses.expires)
-                sql.set_ses(chat.id, ses_id, expires)
-                sesh, exp = sql.get_ses(chat.id)
-        except ValueError:
-            pass
-        try:
-            rep = api_client.think_thought(sesh, query)
-        except Exception:
-            pass
-        async with tbot.action(event.chat_id, 'typing'):           
-              await event.reply(rep)
-        
-
-file_help = os.path.basename(__file__)
-file_help = file_help.replace(".py", "")
-file_helpo = file_help.replace("_", " ")
-
-__help__ = """
- - /addchat: Activates AI mode in the chat the bot will give auto replies to anyone who tags the bot
- - /rmchat: Deactivates AI mode in the chat the bot will stop giving auto replies to anyone who tags the bot
-"""
-
-CMD_HELP.update({file_helpo: [file_helpo, __help__]})
+  if event.is_private:
+     return
+  prof = str(event.text)
+  if event.reply_to_msg_id:
+    reply_msg = await event.get_reply_message()
+    if not reply_msg.sender_id == BOT_ID:
+           return
+  elif "Evie" in prof:
+     pass
+  elif "evie" in prof:
+     pass
+  else:
+     return
+  msg = prof.replace("Evie", "Aco")
+  msg = prof.replace("evie", "Aco")
+  is_chat = sql.is_chat(event.chat_id)
+  if not is_chat:
+         return
+  if msg.startswith("/") or msg.startswith("@") or msg.startswith("."):
+    return
+  lan = translator.detect(msg)
+  if not "en" in lan and not lan == "" and not lan == "ru":
+     test = translator.translate(msg, lang_tgt="en")
+  else:
+     test = msg
+  url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
+  test = test.replace("Evie", "Aco")
+  test = test.replace("evie", "Aco")
+  querystring = {
+            "bid": "178",
+            "key": "sX5A2PcYZbsN5EY6",
+            "uid": "mashape",
+            "msg": {test},
+        }
+  headers = {
+            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+        }
+  response = requests.request("GET", url, headers=headers, params=querystring)
+  result = response.text
+  result = result.replace('{"cnt":"', "")
+  result = result.replace('~', '')
+  result = result.replace('"}', "")
+  result = result.replace("Aco", "Evie")
+  result = result.replace("<a href=\\", "<a href =")
+  result = result.replace("<\/a>", "</a>")
+  if not "en" in lan and not lan == "" and not lan == "ru":
+    finale = translator.translate(result, lang_tgt=lan[0])
+  else:
+    finale = result
+  try:
+    async with tbot.action(event.chat_id, 'typing'):
+           await event.reply(finale)
+  except:
+       await event.reply(lodu)
