4cd05095737ea7d69b819951ecb2420876bf4770
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-16 12:38:33 +0530

    Update Song.py

--- Evie/modules/Song.py
+++ Evie/modules/Song.py
@@ -1,4 +1,4 @@
-from Evie import tbot
+from Evie import tbot, CMD_HELP
 from Evie.events import register
 import asyncio, wget, time, requests, os, json
 from youtube_dl import YoutubeDL
@@ -19,7 +19,7 @@ from youtube_dl.utils import (
 
 
 
-@register(pattern="^/song ?(.*)")
+@register(pattern="^/ytmusic ?(.*)")
 async def yt(event):
  try:
     input_str = event.pattern_match.group(1)
@@ -47,7 +47,7 @@ async def yt(event):
     await asyncio.sleep(0.2)
     downloaded_thumb = wget.download(thumb_url)
     image = Image.open(downloaded_thumb)
-    new_image = image.resize((1920, 1080))
+    new_image = image.resize((1080, 540))
     new_image.save('image69.jpg')
     thumb = './image69.jpg'
     opts = {
@@ -80,9 +80,8 @@ async def yt(event):
     file_stark = f"{ytdl_data['id']}.mp3"
     file=open(file_stark, "rb")
     author = ytdl_data["uploader"]
-    await pablo.edit(f"Preparing to upload song:\n**{vid_title}**\nby **{author}**")
-    async with tbot.action(event.chat_id, 'audio'):
-       await tbot.send_file(
+    await pablo.edit(f"Preparing to upload song:\n**{vid_title}**\nby **{author}**\n**Requested By:** {event.sender.first_name}")
+    await tbot.send_file(
         event.chat_id,
         file,
         thumb=thumb,
@@ -92,8 +91,8 @@ async def yt(event):
                 DocumentAttributeAudio(
                     duration=int(ytdl_data["duration"]),
                     title=str(ytdl_data["title"]),
-                    performer=(ytdl_data["uploader"]),
-                    waveform='256',
+                    performer=author,
+                    waveform='320',
                 )
             ],
     )
@@ -172,27 +171,141 @@ async def deezr(v_url):
         await rkp.edit(f"{str(type(e)): {str(e)}}")
         return
     c_time = time.time()
+    title = rip_data["title"]
     if video:
-        await rkp.edit(f"Sending the video song ...")
-        async with tbot.action(v_url.chat_id, 'video'):
+         await rkp.edit(f"Preparing to Upload Video:\n**{title}**\n**Requested by:** {v_url.sender.first_name}")
          y = await v_url.client.send_file(
             v_url.chat_id,
             f"{rip_data['id']}.mp4",
             supports_streaming=True,
-            attributes=[
-                DocumentAttributeVideo(
-                    duration=int(rip_data["duration"]),
-                    w=800,
-                    h=540,
-             )
-             ],
-            caption=rip_data["title"],
+            caption=rip_data["title"]
         )
-        try:
-            os.system("rm -rf *.mp4")
-            os.system("rm -rf *.webp")
-            os.system("rm -rf *.jpg")
-        except:
-           pass
+         
  except Exception as e:
     await v_url.reply(f'{e}')
+ try:
+   os.system("rm -rf *.mp4")
+   os.system("rm -rf *.webp")
+   os.system("rm -rf *.jpg")
+ except:
+     pass
+
+@register(pattern="^/song ?(.*)")
+async def yt(event):
+    ult = event
+    url = event.pattern_match.group(1)
+    x = await event.reply("Searching...")
+    if not url:
+        return await x.edit("**Error**\nUsage - `.song <song name>`")
+    search = SearchVideos(url, offset=1, mode="json", max_results=1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get("search_result")
+    try:
+        url = q[0]["link"]
+    except BaseException:
+        return await x.edit("`No matching song found...`")
+    type = "audio"
+    await x.edit(f"`Preparing to download {url}...`")
+    if type == "audio":
+        opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                }
+            ],
+            "outtmpl": "%(id)s.mp3",
+            "quiet": True,
+            "logtostderr": False,
+        }
+    try:
+        await x.edit("`Getting info...`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await x.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await x.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await x.edit(
+            "`Video is not available from your geographic location due to"
+            + " geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await x.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await x.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await x.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        return await x.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+    except ExtractorError:
+        return await x.edit("`There was an error during info extraction.`")
+    except Exception as e:
+        return await x.edit(f"{str(type(e)): {str(e)}}")
+    dir = os.listdir()
+    if f"{rip_data['id']}.mp3.jpg" in dir:
+        thumb = f"{rip_data['id']}.mp3.jpg"
+    elif f"{rip_data['id']}.mp3.webp" in dir:
+        thumb = f"{rip_data['id']}.mp3.webp"
+    else:
+        thumb = None
+    upteload = """
+Uploading...
+Song name - **{}**
+By - **{}**
+""".format(
+        rip_data["title"], rip_data["uploader"]
+    )
+    await x.edit(f"{upteload}")
+    async with tbot.action(event.chat_id, 'audio'):
+     await tbot.send_file(
+        ult.chat_id,
+        f"{rip_data['id']}.mp3",
+        thumb=thumb,
+        supports_streaming=True,
+        attributes=[
+            DocumentAttributeAudio(
+                duration=int(rip_data["duration"]),
+                title=str(rip_data["title"]),
+                performer=str(rip_data["uploader"]),
+            )
+        ],
+    )
+    await x.delete()
+    os.remove(f"{rip_data['id']}.mp3")
+    try:
+        os.remove(thumb)
+    except BaseException:
+        pass
+    
+__help__ = """
+- /song <name>: get song from youtube
+- /ytmusic <name>: get song from youtube music
+- /video <name>: gets video song from youtube
+"""
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+CMD_HELP.update({
+    file_helpo: [
+        file_helpo,
+        __help__
+    ]
+})
+
