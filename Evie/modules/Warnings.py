2dd10a97d655e9b3a28dfaeb7135cf73b3334e57
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-17 16:03:26 +0530

    Create Warnings.py

---
+++ Evie/modules/Warnings.py
@@ -0,0 +1,11 @@
+from Evie import tbot, CMD_HELP, MONGO_DB_URI, OWNER_ID, BOT_ID
+from Evie.function import can_change_info, is_admin
+from Evie.events import register
+from telethon import events, Button
+import os
+
+
+@tbot.on(events.NewMessage(pattern="^[!/]warn ?(.*)"))
+async def wn(event):
+ await event.reply(event.raw.text)
+
