7d923a23a7d2ed7017521680f84218776de1eb15
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-17 18:53:47 +0530

    Update Misc.py

--- Evie/modules/Misc.py
+++ Evie/modules/Misc.py
@@ -1,7 +1,7 @@
 import os
 import telethon
 from telethon.tl.functions.users import GetFullUserRequest
-import re, io
+import re, io, traceback
 from telethon import Button, custom, events
 from re import findall
 from urllib.parse import quote
@@ -394,6 +394,55 @@ async def bak(event):
  guy += 1
  buttons = buttons= [Button.inline('Gey {}'.format(guy), data='ghei'), Button.inline('Lesbo', data='leb')]
  await event.edit(buttons=buttons)
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/google-chrome"
+
+@register(pattern="^/webss ?(.*)")
+async def msg(event):
+    input_str = event.pattern_match.group(1)
+    if not input_str:
+      return await event.reply("Please give me a URL to capture it's Screenshot!")
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--test-type")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.binary_location=GOOGLE_CHROME_BIN
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver.get(input_str)
+        k = await event.reply("**Capturing Screenshot..**")
+        height = driver.execute_script(
+            "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
+        )
+        width = driver.execute_script(
+            "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
+        )
+        driver.set_window_size(width + 100, height + 100)
+        im_png = driver.get_screenshot_as_png()
+        driver.close()
+        message_id = event.message.id
+        if event.reply_to_msg_id:
+            message_id = event.reply_to_msg_id
+        await k.edit("**Uploading Screenshot...**")
+        with io.BytesIO(im_png) as out_file:
+            out_file.name = "Evie_sshot.png"
+            await tbot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=False,
+                reply_to=message_id,
+                allow_cache=False,
+                silent=True,
+            )
+        await k.delete()
+    except Exception:
+        await event.reply("Invalid **URL** Provided!")
+
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
