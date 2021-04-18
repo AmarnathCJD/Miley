cbd002098b3e4b42744c737a418f75959d3e133d
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-18 13:16:50 +0530

    Update Misc.py

--- Evie/modules/Misc.py
+++ Evie/modules/Misc.py
@@ -1,7 +1,7 @@
 import os
 import telethon
 from telethon.tl.functions.users import GetFullUserRequest
-import re, io, requests
+import re, io, requests, aiohttp
 from telethon import Button, custom, events
 from re import findall
 from urllib.parse import quote
@@ -44,7 +44,7 @@ async def get_user(event):
             user_object = await tbot.get_entity(user)
             replied_user = await tbot(GetFullUserRequest(user_object.id))
         except (TypeError, ValueError) as err:
-            await event.reply("Failed to get user: unable to getChatMember: Bad Request: user not found")
+            await event.reply("I don't know that user.")
             return None
 
     return replied_user
@@ -470,6 +470,56 @@ async def _(event):
 """
  await event.reply(output)
 
+class AioHttp:
+    @staticmethod
+    async def get_json(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.json()
+
+@register(pattern="^/spwinfo ?(.*)")
+async def sw(event):
+ replied_user = await get_user(event)
+ user_id = replied_user.user.id
+ url = f"https://api.intellivoid.net/spamprotection/v1/lookup?query={user_id}"
+ a = await AioHttp().get_json(url)
+ response = a["success"]
+ if response is True:
+        date = a["results"]["last_updated"]
+        stats = f"**◢ Intellivoid• SpamProtection Info**:\n"
+        stats += f' • **Updated on**: `{datetime.fromtimestamp(date).strftime("%Y-%m-%d %I:%M:%S %p")}`\n'
+        stats += (
+            f" • **Chat Info**: [Link](t.me/SpamProtectionBot/?start=00_{user_id})\n"
+        )
+
+        if a["results"]["attributes"]["is_potential_spammer"] is True:
+            stats += f" • **User**: `USERxSPAM`\n"
+        elif a["results"]["attributes"]["is_operator"] is True:
+            stats += f" • **User**: `USERxOPERATOR`\n"
+        elif a["results"]["attributes"]["is_agent"] is True:
+            stats += f" • **User**: `USERxAGENT`\n"
+        elif a["results"]["attributes"]["is_whitelisted"] is True:
+            stats += f" • **User**: `USERxWHITELISTED`\n"
+
+        stats += f' • **Type**: `{a["results"]["entity_type"]}`\n'
+        stats += (
+            f' • **Language**: `{a["results"]["language_prediction"]["language"]}`\n'
+        )
+        stats += f' • **Language Probability**: `{a["results"]["language_prediction"]["probability"]}`\n'
+        stats += f"**Spam Prediction**:\n"
+        stats += f' • **Ham Prediction**: `{a["results"]["spam_prediction"]["ham_prediction"]}`\n'
+        stats += f' • **Spam Prediction**: `{a["results"]["spam_prediction"]["spam_prediction"]}`\n'
+        stats += f'**Blacklisted**: `{a["results"]["attributes"]["is_blacklisted"]}`\n'
+        if a["results"]["attributes"]["is_blacklisted"] is True:
+            stats += (
+                f' • **Reason**: `{a["results"]["attributes"]["blacklist_reason"]}`\n'
+            )
+            stats += f' • **Flag**: `{a["results"]["attributes"]["blacklist_flag"]}`\n'
+        stats += f'**PTID**:\n`{a["results"]["private_telegram_id"]}`\n'
+        await event.reply(stats, link_preview=False)
+ else:
+    await event.reply("Cannot reach SpamProtection API!")
+
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
@@ -482,5 +532,6 @@ __help__ = """
  - /info: gets info of a user
  - /webss: gets screenshot of a website
  - /iplookup: gets info about an ipaddress
+ - /spwinfo: gets intellvoid spam ban details
 """
 CMD_HELP.update({file_helpo: [file_helpo, __help__]})
