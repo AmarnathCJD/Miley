522c15a770056fe9ab9076c6f1addbf33c065542
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-21 22:33:21 +0530

    Update __init__.py

--- Evie/function/__init__.py
+++ Evie/function/__init__.py
@@ -91,9 +91,11 @@ async def can_ban_users(message):
         isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.ban_users
     )
 
-def sudo(iid):
-  k = iid
-  if sql.is_sudo(k):
+
+
+
+def sudo(user_id):
+  if sql.is_sudo(int(user_id)):
    return True
   else:
    return False
@@ -207,3 +209,31 @@ def gen_captcha(captcha_string_size=10):
     for item in captcha_string_list:
         captcha_string += str(item)
     return captcha_string
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
