f0969040d36bd344340ec8f6692485bb3aeb096a
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-21 17:48:46 +0530

    Update Bans.py

--- Evie/modules/Bans.py
+++ Evie/modules/Bans.py
@@ -1,39 +1,9 @@
 from Evie import tbot, OWNER_ID, BOT_ID, CMD_HELP
 from Evie.events import register
 from Evie.function import is_admin, can_ban_users, bot_ban, get_user
-from telethon.tl.functions.channels import EditBannedRequest
-from telethon.tl.types import ChatBannedRights
 from telethon import events
 import time, os
-from telethon.tl.functions.users import GetFullUserRequest
 import telethon
-from telethon import Button
-
-BANNED_RIGHTS = ChatBannedRights(
-    until_date=None,
-    view_messages=True,
-    send_messages=True,
-    send_media=True,
-    send_stickers=True,
-    send_gifs=True,
-    send_games=True,
-    send_inline=True,
-    embed_links=True,
-)
-
-UNBAN_RIGHTS = ChatBannedRights(
-    until_date=None,
-    send_messages=None,
-    send_media=None,
-    send_stickers=None,
-    send_gifs=None,
-    send_games=None,
-    send_inline=None,
-    embed_links=None,
-)
-
-MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
-UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 
 async def extract_time(message, time_val):
     if any(time_val.endswith(unit) for unit in ("m", "h", "d")):
@@ -43,14 +13,14 @@ async def extract_time(message, time_val):
             await message.reply(f"Invalid time type specified. Expected m,h, or d, got: {unit}")
             return ""
 
-        if unit == "m":
+        if unit == "m" or unit == "minute":
             bantime = int(time.time() + int(time_num) * 60)
-        elif unit == "h":
+        elif unit == "h" or unit == "hour":
             bantime = int(time.time() + int(time_num) * 60 * 60)
-        elif unit == "d":
+        elif unit == "d" or unit == "day":
             bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
         else:
-            return 
+            return
         return bantime
     else:
         await message.reply(
@@ -194,7 +164,7 @@ async def dban(event):
     reason = f'\n**Reason:** {args}'
   else:
     reason = ""
-  await tbot(EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS))
+  await tbot.kick_participant(event.chat_id, user.id)
   await event.reply(f"Kicked!{reason}")
 
 @tbot.on(events.NewMessage(pattern="^[!/]dkick ?(.*)"))
@@ -228,7 +198,7 @@ async def dban(event):
   await zx.delete()
   mk = (await event.get_reply_message()).sender_id
   await tbot.kick_participant(event.chat_id, int(mk))
-  await event.reply(f"Successfully Kicked!{reason}")
+  await event.reply(f"Kicked!{reason}")
 
 @tbot.on(events.NewMessage(pattern="^[!/]dmute ?(.*)"))
 async def dban(event):
@@ -306,9 +276,9 @@ async def dban(event):
     reason = f'\n**Reason:** {args}'
   else:
     reason = ""
-  replied_user = await tbot(GetFullUserRequest(user.id))
+  replied_user = await tbot.get_entity(user.id)
   await tbot.edit_permissions(event.chat_id, user.id, send_messages=False)
-  await event.reply(f"**{replied_user.user.first_name}** is muted in **{event.chat.title}**.{reason}")
+  await event.reply(f"**{replied_user.first_name}** is muted in **{event.chat.title}**.{reason}")
 
 @tbot.on(events.NewMessage(pattern="^[!/]unmute ?(.*)"))
 async def dban(event):
@@ -334,7 +304,7 @@ async def dban(event):
     reason = ""
   replied_user = await tbot.get_entity(user.id)
   await tbot.edit_permissions(event.chat_id, user.id, send_messages=True)
-  await event.reply(f"Yep, **{replied_user.first_name}** can start talking again in **{event.chat.title}**.{reason}")
+  await event.respond(f"Yep, **{replied_user.first_name}** can start talking again in **{event.chat.title}**.{reason}")
 
 @tbot.on(events.NewMessage(pattern="^[!/]smute ?(.*)"))
 async def dban(event):
@@ -355,7 +325,7 @@ async def dban(event):
         return
   if not await bot_ban(message=event):
     return await event.reply("I don't have enough rights to do this!")
-  await tbot(EditBannedRequest(event.chat_id, user.id, MUTE_RIGHTS))
+  await tbot.edit_permissions(event.chat_id, user.id, send_messages=False)
 
 @tbot.on(events.NewMessage(pattern="^[!/]tmute ?(.*)"))
 async def tmute(event):
@@ -384,12 +354,54 @@ async def tmute(event):
  elif len(input) == 1:
    time = input[0]
    reason = None
+ if reason:
+   reason = f"**Reason:** {reason}"
+ else:
+   reason = ""
  if len(time) == 1:
    return await event.reply(f"Invalid time type specified. Expected m,h, or d, got: {time}")
  mutetime = await extract_time(event, time)
  await tbot.edit_permissions(event.chat_id, user.id, send_messages=False, until_date=mutetime)
  replied_user = await tbot.get_entity(user.id)
- await event.respond(f'Muted **{replied_user.first_name}** for {args}!')
+ await event.respond(f'Muted **{replied_user.first_name}** for {args}!\n{reason}')
+
+@tbot.on(events.NewMessage(pattern="^[!/]tban ?(.*)"))
+async def tmute(event):
+ if event.is_private:
+    return await event.reply("This command is made to be used in group chats, not in pm!")
+ user, args = await get_user(event)
+ if not event.sender_id == OWNER_ID:
+    if not await is_admin(event, event.sender_id):
+       return await event.reply("Only Admins can execute this command!")
+    if user:
+      if user.id == BOT_ID or user.id == OWNER_ID:
+        return await event.reply("Ask the chat creator to do it!")
+      if await is_admin(event, user.id):
+        return await event.reply("Yeah lets start banning admins!")
+    if not await can_ban_users(message=event):
+        await event.reply("You don't have enough rights to do that!")
+        return
+ if not await bot_ban(message=event):
+    return await event.reply("I don't have enough rights to do this!")
+ if not args:
+   return await event.reply("You haven't specified a time to ban this user for!")
+ input = args.split(" ", 1)
+ if len(input) == 2:
+   time = input[0]
+   reason = input[1]
+ elif len(input) == 1:
+   time = input[0]
+   reason = None
+ if reason:
+   reason = f"**Reason:** {reason}"
+ else:
+   reason = ""
+ if len(time) == 1:
+   return await event.reply(f"Invalid time type specified. Expected m,h, or d, got: {time}")
+ mutetime = await extract_time(event, time)
+ await tbot.edit_permissions(event.chat_id, user.id, view_messages=False, until_date=mutetime)
+ replied_user = await tbot.get_entity(user.id)
+ await event.respond(f'Banned **{replied_user.first_name}** for {args}!\n{reason}')
 
 @register(pattern="^/kickme$")
 async def pk(event):
@@ -403,7 +415,6 @@ async def pk(event):
  except:
     await event.reply("Failed to kick!")
  
-
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
@@ -431,7 +442,6 @@ This module allows you to do that easily, by exposing some common actions, so ev
 - /kick: Kick a user.
 - /dkick: Kick a user by reply, and delete their message.
 - /skick: Silently kick a user, and delete your message
-**Now Supports Anonymous Admins Also -Soon**
 """
 CMD_HELP.update({
     file_helpo: [
