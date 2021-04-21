0723cfd79fe72bd3c775650efb0776ccdf4de817
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-21 16:37:09 +0530

    Create evlin.py

---
+++ evlin.py
@@ -0,0 +1,13 @@
+from Evie import tbot as lbot, TOKEN, OWNER_ID
+from telethon import events
+
+try:
+    lbot.start(bot_token=TOKEN)
+except Exception:
+    print("failed to start logs module)
+
+@lbot.on(events.NewMessage(pattern="[!/]logs "))
+async def lg(event):
+ if not event.sender_id == OWNER_ID:
+   return
+ await event.reply("Logs Modules Active..")
