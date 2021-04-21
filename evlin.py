483bdf5ac0b369d853c9ee842dc76a8530ff8427
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-21 16:44:22 +0530

    Update evlin.py

--- evlin.py
+++ evlin.py
@@ -1,8 +1,8 @@
-from Evie import tbot, TOKEN, OWNER_ID
+from Evie import ubot, TOKEN, OWNER_ID
 from telethon import events
 
-@tbot.on(events.NewMessage(pattern="!logs"))
+@ubot.on(events.NewMessage(pattern="!logs"))
 async def lg(event):
  if not event.sender_id == OWNER_ID:
    return
- await event.reply("Logs Modules Active..")
+ await event.edit("Logs Modules Active..")
