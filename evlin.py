7e21f12b0e6aaf577ebd1af1adc57361263abb47
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-21 16:46:49 +0530

    Delete evlin.py

--- evlin.py
+++
@@ -1,8 +0,0 @@
-from Evie import ubot, TOKEN, OWNER_ID
-from telethon import events
-
-@ubot.on(events.NewMessage(pattern="!logs"))
-async def lg(event):
- if not event.sender_id == OWNER_ID:
-   return
- await event.edit("Logs Modules Active..")
