723812a805fc90be65585ab4156791a3d8502a6e
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-05 12:58:47 +0530

    Update events.py

--- Evie/events.py
+++ Evie/events.py
@@ -95,6 +95,19 @@ def register(**args):
 
     return decorator
 
+def Ebot(**args):
+    """New message."""
+    pattern = args.get('pattern', None)
+    r_pattern = r'^[/?!.]'
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+    args['pattern'] = pattern.replace('^/', r_pattern, 1)
+
+    def decorator(func):
+        tbot.add_event_handler(func, events.NewMessage(**args))
+        return func
+
+    return decorator
 
 def eviebot(**args):
     pattern = args.get("pattern", None)
