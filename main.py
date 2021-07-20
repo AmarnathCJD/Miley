97129341bac7118d99dd16c32842cd510022b97a
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-20 22:09:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,8 @@ async def play_cb_(e):
  x = await e.edit(f"Downloading **{song_name}** Now!")
  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([song_id])
- await x.edit(x_info.format(song_id, song_name, song.get("duration"), e.sender.first_name), parse_mode="html")
+ buttons = [[Button.url("⏸️", data="pause"), Button.url("⏭️", data="next"), Button.inline("⏹️", data="stop")], [Button.inline("➕ Group Playlist", data="group_playlist")], [Button.inline("➕ Personal Playlist", data="my_playlist")], [Button.inline("🗑️ Close Menu", data="close_menu")],]
+ await x.edit(x_info.format(song_id, song_name, song.get("duration"), e.sender.first_name), parse_mode="html", buttons=buttons)
 
 
 
