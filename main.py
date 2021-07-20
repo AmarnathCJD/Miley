51b66104d556910f70749470e75d97dfd7907cf3
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-20 21:37:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,18 +42,28 @@ async def playvc(e):
  buttons.append([Button.inline("🗑️ Close Menu", data="close_menu|" + str(e.sender_id))])
  await e.respond(final_text, buttons=buttons, file=kdawg[0].get("thumbnails")[4], parse_mode="html")
 
+x_info = """
+🎥<b>Playing:</b> <a href="https://www.youtube.com/watch?v={}">{}</a>
+⏳<b>Duration:</b> {}
+💡<b>Info:</b> <a href="https://t.me/missneko_bot?start=help">Get Additional Information</a>
+👤<b>Requested by:</b> {}
+"""
+
 @bot.on(events.CallbackQuery(pattern=r"play(\_(.*))"))
 async def play_cb_(e):
  song_id, sender_id = (((e.pattern_match.group(1)).decode()).split("_", 1)[1]).split("|", 1)
  if not int(sender_id) == e.sender_id:
     return await e.answer("This is not for you!", alert=True)
  song_id = song_id.strip()
- song_name = ((SearchVideos (song_id, max_results=1, mode="dict")).result()["search_result"])[0].get("title")
- x = await e.edit("Downloading **{song_name}** Now!")
+ song = ((SearchVideos (song_id, max_results=1, mode="dict")).result()["search_result"])[0]
+ song_name = song.get("title")
+ x = await e.edit(f"Downloading **{song_name}** Now!")
  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([song_id])
- await x.edit("song downloaded")
- remove(f"{song_id}.mp3")
+ await x.edit(x_info.format(song_id, song_name, song.get("duration"), e.sender.first_name))
+
+
+
  
 
 
