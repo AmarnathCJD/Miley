05d8ee928bd47e555b324c1b24899335d78784b6
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-22 10:49:07 +0530

    Create __init__.py

---
+++ Miley/__init__.py
@@ -0,0 +1,18 @@
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from os import environ as e
+
+API_KEY = e.get("API_KEY")
+API_HASH = e.get("API_HASH")
+STRING_SESSION = e.get("STRING_SESSION")
+OWNER_ID = int(e.get("OWNER_ID"))
+TOKEN = e.get("TOKEN")
+
+bot = TelegramClient (None, API_KEY, API_HASH)
+vc = TelegramClient (StringSession (STRING_SESSION), API_KEY, API_HASH)
+
+try:
+ vc.start()
+except BaseException as x:
+ print(x)
+
