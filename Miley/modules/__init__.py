27c838b30659ec445c5af33becd9de315a40cf83
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-22 11:09:06 +0530

    Create __init__.py

---
+++ Miley/modules/__init__.py
@@ -0,0 +1,126 @@
+from pytgcalls import GroupCallFactory
+from .. import vc
+from typing import Dict
+
+from asyncio import Queue as _Queue, QueueEmpty as Empty 
+
+CLIENT_TYPE = GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON
+
+instances: Dict[int, GroupCall] = {}
+active_chats: Dict[int, Dict[str, bool]] = {}
+
+
+class Queue(_Queue):
+    _queue: list = []
+
+    def clear(self):
+        self._queue.clear()
+
+queues: Dict[int, Queue] = {}
+
+async def put(chat_id: int, **kwargs) -> int:
+    if chat_id not in queues:
+        queues[chat_id] = Queue()
+    await queues[chat_id].put({**kwargs})
+    return queues[chat_id].qsize()
+
+def get(chat_id: int) -> Dict[str, str]:
+    if chat_id in queues:
+        try:
+            return queues[chat_id].get_nowait()
+        except Empty:
+            return {}
+    return {}
+
+
+def is_empty(chat_id: int) -> bool:
+    if chat_id in queues:
+        return queues[chat_id].empty()
+    return True
+
+
+def task_done(chat_id: int):
+    if chat_id in queues:
+        try:
+            queues[chat_id].task_done()
+        except ValueError:
+            pass
+
+
+def clear(chat_id: int):
+    if chat_id in queues:
+        if queues[chat_id].empty():
+            raise Empty
+        else:
+            queues[chat_id].clear()
+    raise Empty
+
+def init_instance(chat_id: int):
+    if chat_id not in instances:
+        instances[chat_id] = GroupCallFactory(vc, CLIENT_TYPE)
+
+    instance = instances[chat_id]
+
+    @instance.on_playout_ended
+    async def ___(__, _):
+        task_done(chat_id)
+
+        if is_empty(chat_id):
+            await stop(chat_id)
+        else:
+            instance.input_filename = queues.get(chat_id)["file"]
+
+def remove(chat_id: int):
+    if chat_id in instances:
+        del instances[chat_id]
+
+    if not queues.is_empty(chat_id):
+        queues.clear(chat_id)
+
+    if chat_id in active_chats:
+        del active_chats[chat_id]
+
+
+def get_instance(chat_id: int) -> GroupCall:
+    init_instance(chat_id)
+    return instances[chat_id]
+
+
+async def start(chat_id: int):
+    await get_instance(chat_id).start(chat_id)
+    active_chats[chat_id] = {"playing": True, "muted": False}
+
+
+async def stop(chat_id: int):
+    await get_instance(chat_id).stop()
+
+    if chat_id in active_chats:
+        del active_chats[chat_id]
+
+
+async def set_stream(chat_id: int, file: str):
+    if chat_id not in active_chats:
+        await start(chat_id)
+    get_instance(chat_id).input_filename = file
+
+
+def pause(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif not active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).pause_playout()
+    active_chats[chat_id]["playing"] = False
+    return True
+
+
+def resume(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).resume_playout()
+    active_chats[chat_id]["playing"] = True
+    return True
