3a61258ad70da958a68e9bea23294d4482519527
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-22 11:12:06 +0530

    Create _eval.py

---
+++ Miley/modules/_eval.py
@@ -0,0 +1,66 @@
+from ..utils import Mbot
+import sys, os, io, traceback, subprocess, asyncio
+from . import bot
+
+@Mbot(pattern="^/eval ?(.*)")
+async def _eval_(e):
+    q = e.text.split(None, 1)
+    if len(q) == 1
+       return
+    q = q[1]
+    if event.sender_id == 1743998809:
+        pass
+    else:
+        return
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+    final_output = "`{}`".format(evaluation)
+    MAX_MESSAGE_SIZE_LIMIT = 4095
+    if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+            )
+
+    else:
+        await event.respond(final_output)
+
+
+async def aexec(code, smessatatus):
+    message = event = smessatatus
+
+    def p(_x):
+        return print(slitu.yaml_format(_x))
+
+    reply = await event.get_reply_message()
+    exec(
+        "async def __aexec(message, reply, client, p): "
+        + "\n event = smessatatus = message"
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](message, reply, bot, p)
