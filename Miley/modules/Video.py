0c487e417c0c5464dc802c3ddfb2ff48bb642a3f
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-11 10:12:46 +0530

    Update Video.py

--- Miley/modules/Video.py
+++ Miley/modules/Video.py
@@ -1,6 +1,8 @@
 import yt_dlp
 from .. import vc
 from ..utils import Cbq, Mbot
+from youtubesearchpython import VideosSearch as vs
+
 from . import (
     active_chats,
     can_manage_call,
@@ -14,7 +16,7 @@ from . import (
     task_done,
 )
 
-@Cbq(pattern="^/play ?(.*)")
+@Cbq(pattern="^/play (audio|video)?(.*)")
 async def _play(e):
  r = await e.get_reply_message()
  if r and r.video or r.audio:
@@ -24,7 +26,12 @@ async def _play(e):
    q = e.text.split(" ", 1)[1]
   except IndexError:
    return await e.reply("Qoery was not found.")
+  q = q.replace(("audio", "video"), "")
   if q.startswith("http"):
    url = q
   else:
-   
+   try:
+        v = vs(q, limit=1).result()["result"][0]
+   except (IndexError, KeyError, TypeError):
+        return await e.reply("No song/video result found for your query!")
+  print("soon")
